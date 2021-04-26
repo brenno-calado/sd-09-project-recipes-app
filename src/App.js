e2821547c2ccb03b1baf0c248d963a0bf5455efc
tiago-yoneda <xyonedax@gmail.com>
2021-04-26 19:22:13 -0300

    Feitos requisitos 13 a 18 -17

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Bebidas from './pages/Bebidas';
+import BebidasDetails from './pages/BebidasDetails';
+import Comidas from './pages/Comidas';
+import ComidasDetails from './pages/ComidasDetails';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -9,7 +13,10 @@ function App() {
     <div className="meals">
       <BrowserRouter>
         <Switch>
-          <Route path="/comidas" />
+          <Route path="/bebidas/:id" component={ BebidasDetails } />
+          <Route path="/bebidas" component={ Bebidas } />
+          <Route path="/comidas/:id" component={ ComidasDetails } />
+          <Route path="/comidas" component={ Comidas } />
           <Route path="/" component={ Login } />
         </Switch>
       </BrowserRouter>
