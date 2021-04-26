2fc64f44e996380515b87703cbbb214d5bf3f92f
Max <gmaxgomes@gmail.com>
2021-04-26 17:52:35 -0300

    req 2, 3, 4 e 5

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
 import { combineReducers } from 'redux';
+import userReducer from './userReducer';
 
-const rootReducer = combineReducers({ });
+
+const rootReducer = combineReducers({
+    userReducer,
+ });
 
 export default rootReducer;
