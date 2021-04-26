bc994962b826fbba596909688f2f129ef39b0847
BBrolesi <brunohsbrolesi@gmail.com>
2021-04-26 18:39:25 -0300

    feat: add drinks icon

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
 
 function BottomNav() {
   return (
     <nav data-testid="footer">
       <h1>BottomNav</h1>
+      <Link to="/bebidas">
+        <img
+          src={ drinkIcon }
+          alt="Icone de Drink"
+          data-testid="drinks-bottom-btn"
+        />
+      </Link>
     </nav>
   );
 }
