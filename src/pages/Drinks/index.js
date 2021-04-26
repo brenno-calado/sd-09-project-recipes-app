9aa6bafb12c0cb3561363f54777c08917f082cf5
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-04-26 15:54:01 -0300

    Organizaçao de pasta e arquivos do projeto

---
+++ src/pages/Drinks/index.js
@@ -0,0 +1,3 @@
+export { default as Drink } from './Drinks';
+export { default as DrinkDetails } from './DrinkDetails';
+export { default as DrinkProgress } from './DrinkProgress';
---
+++ src/pages/Explorer/index.js
@@ -0,0 +1,1 @@
+/* Index de exportação do Explorer */
