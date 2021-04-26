6f432baaa86e2516cf6ef80b45ce6f4d4a97067c
Layo Kaminski <layokaminski@gmail.com>
2021-04-26 16:17:47 -0300

    Ajustes de Lint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './pages/Login';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Login from './pages/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
       <BrowserRouter>
         <Switch>
-          <Route path="/comidas"/>
+          <Route path="/comidas" />
           <Route path="/" component={ Login } />
         </Switch>
       </BrowserRouter>
