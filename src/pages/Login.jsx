a6c0848c64ed488cb6f8d49354db93d18e1367c6
Carine Becker <caarine.m@gmail.com>
2021-04-26 18:33:32 -0300

    feat: req2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+
 
 function Login() {
   return (
-    <h1>Login page</h1>
+    <main>
+      <Container fluid>
+        <h1>Login</h1>
+        <Form>
+          <Form.Group>
+            <Form.Label>E-mail</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Insira seu email"
+              data-testid="email-input"
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Senha</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Insira sua senha"
+              data-testid="password-input"
+            />
+          </Form.Group>
+          <Button
+            type="submit"
+            data-testid="login-submit-btn"
+          >
+            Entrar
+          </Button>
+        </Form>
+      </Container>
+    </main>
   );
 }
 
