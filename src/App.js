3b4de19624a0b7d685334c1d11827dd78dd2b75c
Pedro Henrique <pedro.876.ph@gmail.com>
2021-04-26 18:16:50 -0300

    tela de login concluida

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './Pages/Login';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE!</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
