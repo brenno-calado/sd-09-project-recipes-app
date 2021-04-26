ffac452d272fdc4acb5cbf2305082030d9091887
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-04-26 18:31:01 -0300

    Task 5

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [buttonOff, setButtonOff] = useState(true);
+
+  useEffect(() => {
+    const validateEmail = /^[\w.]+@[a-z]+\.\w{2,3}$/g;
+    const passwordLength = password.length;
+    const minPasswordLength = 6;
+    if (validateEmail.test(email) && passwordLength > minPasswordLength) {
+      setButtonOff(false);
+    } else {
+      setButtonOff(true);
+    }
+  }, [password, email]);
+
+  function handleChangeEmail({ target }) {
+    setEmail(target.value);
+  }
+
+  function handleChangePassword({ target }) {
+    setPassword(target.value);
+  }
+
   return (
     <div>
-      <input type="email" data-testid="email-input" />
-      <input type="email" data-testid="password-input" />
-      <button type="button" data-testid="login-submit-btn">Entrar</button>
+      <input
+        onChange={ handleChangeEmail }
+        type="email"
+        data-testid="email-input"
+      />
+      <input
+        onChange={ handleChangePassword }
+        type="password"
+        min="6"
+        data-testid="password-input"
+      />
+      <button
+        disabled={ buttonOff }
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Entrar
+      </button>
     </div>
   );
 }
