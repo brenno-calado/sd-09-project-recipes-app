544af901c5cffd4ca95a6ddec82ab1a10c03fb2d
Enio Nicael <enionicael@gmail.com>
2021-04-26 18:35:19 -0300

    Feat: requisito 19, adiciona os icones do menu inferior.

--- src/components/BottomMenu.jsx
+++ src/components/BottomMenu.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+
 const BottomMenu = () => {
   return (
-    <p>Bottom Menu</p>
+    <footer data-testid="footer">
+      <img src={ drinkIcon } alt="Drink Icon" data-testid="drinks-bottom-btn"/>
+      <img src={ mealIcon } alt="Meal Icon" data-testid="explore-bottom-btn"/>
+      <img src={ exploreIcon } alt="Explore Icon" data-testid="food-bottom-btn"/>
+    </footer>
   );
 };
 
