75b192368abb5e56a1af40ae1dcd9670b7a07658
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-04-26 17:03:57 -0400

    Requisito 12 - Adiciona SearchBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { string, bool } from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
 
 function Header({ page, search }) {
+  const [searchBar, setSearchBar] = useState(false);
+
+  const HandleSearch = () => {
+    if (searchBar) {
+      setSearchBar(false);
+    } else {
+      setSearchBar(true);
+    }
+  };
+
   return (
     <div>
       <Link to="/perfil">
         <img src={ profileIcon } alt="Profile" data-testid="profile-top-btn" />
       </Link>
       <h2 data-testid="page-title">{ page }</h2>
-      { search && <img src={ searchIcon } alt="Search" data-testid="search-top-btn" /> }
+      { search
+      && (
+        <button type="button" onClick={ HandleSearch }>
+          <img src={ searchIcon } alt="Search" data-testid="search-top-btn" />
+        </button>
+      )}
+      { searchBar && <SearchBar /> }
     </div>
   );
 }
