8612bf812f16247647d5d27a6e6a571d2c15f3f4
Cainan <cainan6697@gmail.com>
2021-04-26 16:35:10 -0300

    Recipes App Group-28

---
+++ src/pages/Login.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div className="login-container">
+      <h1>Login</h1>
+      <input
+        type="email"
+        className="form-control"
+        placeholder="email"
+      />
+      <input
+        type="password"
+        className="form-control"
+        placeholder="senha"
+      />
+      <button
+        type="button"
+        style={ { width: 300 } }
+        className="btn btn-primary"
+      >
+        Entrar
+      </button>
+    </div>
+  );
+}
+
+export default Login;
