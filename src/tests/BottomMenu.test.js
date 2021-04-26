21aa49b094b010c4b57d7b3011c55a969e8947fe
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-04-26 20:08:18 -0300

    Add tests to footer element

--- src/tests/BottomMenu.test.js
+++ src/tests/BottomMenu.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import renderWithRouter from './helpers/renderWithRouter';
 import BottomMenu from '../components/BottomMenu';
 
