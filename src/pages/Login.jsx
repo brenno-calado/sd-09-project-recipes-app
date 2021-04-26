16fd5e70291637651a85bd4e890c6bc0f690bebf
Cainan <cainan6697@gmail.com>
2021-04-26 17:54:23 -0300

    Requisitos 2, 3 e 4 feitos

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <div className="login-container">
-      <h1>Login 2.0</h1>
+    <div className="meals login-container">
+      <h1>Recipes App</h1>
       <input
         type="email"
+        data-testid="email-input"
+        style={ { width: 300, marginBottom: 5 } }
         className="form-control"
         placeholder="email"
       />
       <input
         type="password"
+        data-testid="password-input"
+        style={ { width: 300, marginBottom: 5 } }
         className="form-control"
         placeholder="senha"
       />
       <button
         type="button"
+        disabled
+        data-testid="login-submit-btn"
         style={ { width: 300 } }
         className="btn btn-primary"
       >
