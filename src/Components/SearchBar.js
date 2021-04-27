c142f220a363b0f968214db103dde02095c6b456
Sabrina Alves Delpache <sasa2605.alves@gmail.com>
2021-04-27 16:03:31 -0300

    Requisitos 11 e 12

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -9,29 +9,31 @@ function SearchBar() {
         plceholder="Buscar Receita"
       />
       <label htmlFor="ingredient-radio">
-        Ingrediente:
         <input
-          type="text"
+          type="radio"
           id="ingredient-radio"
           name="radio-search"
           data-testid="ingredient-search-radio"
         />
+        Ingrediente
       </label>
       <label htmlFor="name-radio">
         <input
-          type="text"
+          type="radio"
           id="name-radio"
           name="radio-search"
           data-testid="name-search-radio"
         />
+        Nome
       </label>
       <label htmlFor="first-letter-radio">
         <input
-          type="text"
+          type="radio"
           id="first-letter-radio"
           name="radio-search"
           data-testid="first-letter-search-radio"
         />
+        Primeira-letra
       </label>
       <button
         type="button"
