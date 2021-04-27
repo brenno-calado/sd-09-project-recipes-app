56c5974843aeeaf99542957a763de291bc5c29ee
Marina Alane <marina.alane@outlook.com>
2021-04-27 17:47:16 -0300

    req. 82, 84, 85 e 86 - adc botoes e rotas na tela de perfil

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,6 +7,35 @@ function Perfil() {
   return (
     <div>
       <Header page="Perfil" />
+      <p data-testid="profile-email"> Email</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        <Link
+          to="/receitas-feitas"
+        >
+          Receitas Feitas
+        </Link>
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        <Link
+          to="/receitas-favoritas"
+        >
+          Receitas Favoritas
+        </Link>
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Sair
+      </button>
       Perfil
       <Footer />
     </div>
