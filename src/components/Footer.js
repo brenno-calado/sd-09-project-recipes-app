1748064cbe7e1caf566875f2ac6199eed19cb728
MarcosAlbuquerque <mpdsa20@gmail.com>
2021-04-27 19:05:01 -0300

    Requisito 21, depende dos anteriores

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import '../App.css';
+import { Link } from 'react-router-dom';
+import React from 'react';
+import exploreIcon from '../images/exploreIcon.svg';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+function footer() {
+  // const [foods, setFoods] = useState('');
+
+  return (
+    <footer id="optionsPerfil" data-testid="footer">
+      <Link to="/bebidas">
+        <img src={ drinkIcon } alt="Bebidas" data-testid="drinks-bottom-btn" />
+      </Link>
+      <Link to="/explorar">
+        <img src={ exploreIcon } alt="Explorar" data-testid="explore-bottom-btn" />
+      </Link>
+      <Link to="/comidas">
+        <img src={ mealIcon } alt="Comidas" data-testid="food-bottom-btn" />
+      </Link>
+    </footer>
+  );
+}
+
+export default footer;
