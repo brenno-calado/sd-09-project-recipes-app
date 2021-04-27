d2544d9b12ee3e9a951ae2f76e8ac811e36b8985
Samuel <samuelvanguarda@gmail.com>
2021-04-27 19:48:48 -0300

    [FEAT][Requisitos 82 ao 87][Tela de Perfil]

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Profile = () => (
-  <div>
-    Profile
-  </div>
-);
+const Profile = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const { email } = user;
+
+  const clearLocalStorage = () => {
+    localStorage.clear();
+  };
+
+  return (
+    <div>
+      <p data-testid="profile-email">{ email }</p>
+      <Link to="/receitas-feitas">
+        <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ clearLocalStorage }
+        >
+          Sair
+        </button>
+      </Link>
+    </div>
+  );
+};
 
 export default Profile;
