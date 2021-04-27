e81580ec7a702df35411af64b3783dbb3450a482
saulokirchmaier <saulokirchmaier@gmail.com>
2021-04-27 18:10:27 -0300

    EX. 11 - link nomenu para pagina profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { string, bool } from 'prop-types';
 import './Header.css';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ title, search = false }) {
+  // const handleProfileClick = () => (<Redirect to="/perfil" />);
+
+  const handleSearchClick = ({ target }) => {
+    console.log(target);
+  };
+
   const renderSearchButton = () => (
-    <button type="button" data-testid="search-top-btn" src="../images/searchIcon.svg">
-      <img src={ searchIcon } alt="profile" />
+    <button
+      type="button"
+      data-testid="search-top-btn"
+      src="../images/searchIcon.svg"
+      onClick={ handleSearchClick }
+    >
+      <img src={ searchIcon } alt="search" />
     </button>
   );
 
   return (
     <header className="Header">
-      <button type="button" data-testid="profile-top-btn" src="'../images/profileIcon.svg'">
-        <img src={ profileIcon } alt="profile" />
-      </button>
+      <Link to="/perfil">
+        <button
+          type="button"
+          data-testid="profile-top-btn"
+          src="../images/profileIcon.svg"
+        >
+          <img src={ profileIcon } alt="profile" />
+        </button>
+      </Link>
       <h3 data-testid="page-title">{ title }</h3>
       {search && renderSearchButton()}
     </header>
   );
 }
+
+Header.propTypes = {
+  title: string,
+  search: bool,
+}.isRequired;
 
 export default Header;
