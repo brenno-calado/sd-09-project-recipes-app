7ceb26b7598d16edda112cfd3e1f9a9de32213c3
Luciano_Lodi <lrlodi@gmail.com>
2021-04-27 17:43:55 -0300

    Atualizacao de requisito 9 a 12

--- src/Common/Components/Header.jsx
+++ src/Common/Components/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ReactComponent as ProfileIcon } from '../../images/profileIcon.svg';
+// import { ReactComponent as ProfileIcon } from '../../images/profileIcon.svg';
 // import { ReactComponent as SearchIcon } from '../../images/searchIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
+import profileIcon from '../../images/profileIcon.svg';
 
 class Header extends React.Component {
   render() {
     return (
       <nav className="header" data-testid="header-component">
-        <Link to="/perfil" data-testid="profile-top-btn">
+        {/* <Link to="/perfil" data-testid="profile-top-btn">
           <ProfileIcon />
         </Link>
         <h1 data-testid="page-title">Page Title</h1>
@@ -19,7 +20,12 @@ class Header extends React.Component {
           onClick={ () => console.log('clicou') }
         >
           <img src={ searchIcon } alt="search-icon" />
-        </button>
+        </button> */}
+        <Link to="/perfil" >
+          <img src={ profileIcon } alt="profile-icon" data-testid="profile-top-btn" />
+        </Link>
+        <h1 data-testid="page-title">Page Title</h1>
+        <img src={ searchIcon } alt="search-icon" data-testid="search-top-btn" />
       </nav>
     );
   }
