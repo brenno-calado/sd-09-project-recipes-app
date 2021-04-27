c661e69b74c2c1a844697102511d574b1ae30103
Luciano_Lodi <lrlodi@gmail.com>
2021-04-27 13:24:39 -0300

    Remoçao do BrowserRouter duplicado e ajuste de teste de rota para pagina do perfil

--- src/Common/Components/Header.jsx
+++ src/Common/Components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { ReactComponent as ProfileIcon } from '../../images/profileIcon.svg';
 // import { ReactComponent as SearchIcon } from '../../images/searchIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
@@ -7,22 +7,20 @@ import searchIcon from '../../images/searchIcon.svg';
 class Header extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <nav className="header" data-testid="header-component">
-          <Link to="/perfil" data-testid="profile-top-btn">
-            <ProfileIcon />
-          </Link>
-          <h1 data-testid="page-title">Page Title</h1>
-          <button
-            type="button"
-            name="search-top-btn"
-            data-testid="search-top-btn"
-            onClick={ () => console.log('clicou') }
-          >
-            <img src={ searchIcon } alt="search-icon" />
-          </button>
-        </nav>
-      </BrowserRouter>
+      <nav className="header" data-testid="header-component">
+        <Link to="/perfil" data-testid="profile-top-btn">
+          <ProfileIcon />
+        </Link>
+        <h1 data-testid="page-title">Page Title</h1>
+        <button
+          type="button"
+          name="search-top-btn"
+          data-testid="search-top-btn"
+          onClick={ () => console.log('clicou') }
+        >
+          <img src={ searchIcon } alt="search-icon" />
+        </button>
+      </nav>
     );
   }
 }
--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import RenderWithRouter from '../Common/RenderWithRouter';
 import Header from '../Common/Components/Header';
+import App from '../App';
 
 // criar testes para o componente header
 
 describe('Testando componente Header', () => {
   it(`9 - Implemente os elementos do header na tela principal de receitas,
   respeitando os atributos descritos no protótipo`, () => {
-    const { getByTestId } = render(<Header />);
+    const { getByTestId } = RenderWithRouter(<Header />);
     expect(getByTestId('profile-top-btn')).toBeInTheDocument();
     expect(getByTestId('search-top-btn')).toBeInTheDocument();
     expect(getByTestId('page-title')).toBeInTheDocument();
@@ -23,9 +24,9 @@ describe('Testando componente Header', () => {
 
   it(`11 - Redirecione a pessoa usuária para a tela
     de perfil ao clicar no botão de perfil`, () => {
-    const { getByTestId, findByText } = RenderWithRouter(<Header />);
+    const { getByTestId, getByText } = RenderWithRouter(<App />);
     userEvent.click(getByTestId('profile-top-btn'));
-    expect(findByText('Profile')).toBeInTheDocument();
+    expect(getByText('Profile')).toBeInTheDocument();
   });
 
   it(`12 - Desenvolva o botão de busca que, ao ser clicado, a barra de busca deve aparecer
