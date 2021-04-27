9d4d8afe273b0317287674e41e2a099baea3610b
Bob <wend3llsouza@gmail.com>
2021-04-27 20:59:31 -0300

    :package: import initial structure pages on App Component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import Layout from './components/Layout';
 import Foods from './pages/Foods';
 import Drinks from './pages/Drinks';
 import FoodDetails from './pages/FoodDetails';
+import FoodProgress from './pages/FoodProgress';
+import DrinkProgress from './pages/DrinkProgress';
+import Explore from './pages/Explore';
+import ExploreFood from './pages/ExploreFood';
+import ExploreDrinks from './pages/ExploreDrinks';
+import ExploreFoodIncredients from './pages/ExploreFoodIncredients';
+import ExploreDrinksIncredients from './pages/ExploreDrinksIncredients';
+import ExploreFoodArea from './pages/ExploreFoodArea';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 
 export default function App() {
   return (
@@ -18,7 +29,7 @@ export default function App() {
             <Route path="/bebidas" component={ Drinks } />
             <Route path="/comidas/:id" component={ FoodDetails } />
             <Route path="/comidas/:id/in-progress" component={ FoodProgress } />
-            { /* <Route path="/bebidas/:id/in-progress" component={ DrinkProgress } />
+            <Route path="/bebidas/:id/in-progress" component={ DrinkProgress } />
             <Route path="/explorar" component={ Explore } />
             <Route path="/explorar/comidas" component={ ExploreFood } />
             <Route path="/explorar/bebidas" component={ ExploreDrinks } />
@@ -31,9 +42,9 @@ export default function App() {
               component={ ExploreDrinksIncredients }
             />
             <Route path="/explorar/comidas/area" component={ ExploreFoodArea } />
-            <Route path="/perfil" component={ profile } />
-            <Route path="/receitas-feitas" component={ doneRecipes } />
-            <Route path="/receitas-favoritas" component={ favoriteRecipes } /> */}
+            <Route path="/perfil" component={ Profile } />
+            <Route path="/receitas-feitas" component={ DoneRecipes } />
+            <Route path="/receitas-favoritas" component={ FavoriteRecipes } />
           </Switch>
         </BrowserRouter>
       </Provider>
