074ec1aa27069da424ba780a64d28753e8e0f4b9
Bob <wend3llsouza@gmail.com>
2021-04-27 20:35:21 -0300

    :checkered_flag: Initial structure pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Login from './pages/Login';
 import { Provider } from './context';
 import Layout from './components/Layout';
 import Foods from './pages/Foods';
+import Drinks from './pages/Drinks';
+import FoodDetails from './pages/FoodDetails';
 
 export default function App() {
   return (
@@ -13,10 +15,10 @@ export default function App() {
           <Switch>
             <Route exact path="/" component={ Login } />
             <Route path="/comidas" component={ Foods } />
-            {/* <Route path="/bebidas" component={ Drinks } />
+            <Route path="/bebidas" component={ Drinks } />
             <Route path="/comidas/:id" component={ FoodDetails } />
             <Route path="/comidas/:id/in-progress" component={ FoodProgress } />
-            <Route path="/bebidas/:id/in-progress" component={ DrinkProgress } />
+            { /* <Route path="/bebidas/:id/in-progress" component={ DrinkProgress } />
             <Route path="/explorar" component={ Explore } />
             <Route path="/explorar/comidas" component={ ExploreFood } />
             <Route path="/explorar/bebidas" component={ ExploreDrinks } />
