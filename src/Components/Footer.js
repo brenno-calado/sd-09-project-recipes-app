e9749b98ab420a9d61907a932505bbf97ddc1d1e
Mateus <mateussantiago.victor@gmail.com>
2021-04-27 10:48:54 -0300

    Requisitos footer incompletos

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,31 +7,29 @@ import mealIcon from '../images/mealIcon.svg';
 class Footer extends Component {
   render() {
     return (
-      <div>
-        <footer data-testid="footer">
-          <Link to="/bebidas">
-            <img
-              src={ drinkIcon }
-              alt="drink-icon"
-              data-testid="drinks-bottom-btn"
-            />
-          </Link>
-          <Link to="/explorar">
-            <img
-              src={ exploreIcon }
-              alt="explore-icon"
-              data-testid="explore-bottom-btn"
-            />
-          </Link>
-          <Link to="/comidas">
-            <img
-              src={ mealIcon }
-              alt="meal-icon"
-              data-testid="food-bottom-btn"
-            />
-          </Link>
-        </footer>
-      </div>
+      <footer data-testid="footer">
+        <Link to="/bebidas">
+          <img
+            src={ drinkIcon }
+            alt="drink-icon"
+            data-testid="drinks-bottom-btn"
+          />
+        </Link>
+        <Link to="/explorar">
+          <img
+            src={ exploreIcon }
+            alt="explore-icon"
+            data-testid="explore-bottom-btn"
+          />
+        </Link>
+        <Link to="/comidas">
+          <img
+            src={ mealIcon }
+            alt="meal-icon"
+            data-testid="food-bottom-btn"
+          />
+        </Link>
+      </footer>
     );
   }
 }
