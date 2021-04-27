550b491fa4a0ce062310f276b140521a59c59382
Pablo Fernandes <pabloprates@gmail.com>
2021-04-27 20:36:23 -0300

    [Requirement 67-68-69][fixing routes]

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () => (
+  <main className="header">
+    Header
+  </main>
+);
+export default Header;
---
+++ src/components/Menu.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Menu = () => (
+  <main className="menu">
+    Bottom menu
+  </main>
+);
+export default Menu;
