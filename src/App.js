1dc7f6f702a54c17ced0187afbfc6d31d9d4f847
Bob <wend3llsouza@gmail.com>
2021-04-27 16:50:49 -0300

    :package: Route | initial structure routes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,26 @@ export default function App() {
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={ Login } />
+            {/* <Route path="/comidas" component={ Foods } />
+            <Route path="/bebidas" component={ Drinks } />
+            <Route path="/comidas/:id" component={ FoodDetails } />
+            <Route path="/comidas/:id/in-progress" component={ FoodProgress } />
+            <Route path="/bebidas/:id/in-progress" component={ DrinkProgress } />
+            <Route path="/explorar" component={ Explore } />
+            <Route path="/explorar/comidas" component={ ExploreFood } />
+            <Route path="/explorar/bebidas" component={ ExploreDrinks } />
+            <Route
+              path="/explorar/comidas/ingredientes"
+              component={ ExploreFoodIncredients }
+            />
+            <Route
+              path="/explorar/bebidas/ingredientes"
+              component={ ExploreDrinksIncredients }
+            />
+            <Route path="/explorar/comidas/area" component={ ExploreFoodArea } />
+            <Route path="/perfil" component={ perfil } />
+            <Route path="/receitas-feitas" component={ doneRecipes } />
+            <Route path="/receitas-favoritas" component={ favoriteRecipes } /> */}
           </Switch>
         </BrowserRouter>
       </Provider>
