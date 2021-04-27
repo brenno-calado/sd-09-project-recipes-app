8caafd5d061a829ec1739385557089e6787edd86
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-04-27 14:38:35 -0400

    Adiciona requisito 15 e 16

--- src/pages/DrinkMainPage.jsx
+++ src/pages/DrinkMainPage.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { object } from 'prop-types';
+import { connect } from 'react-redux';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import RecipesList from '../components/RecipesList';
+import { searchBarFetch } from '../actions/searchBar';
 
-function DrinkMainPage({ match }) {
+function DrinkMainPage({ match, setRecipes }) {
+  useEffect(() => {
+    setRecipes({ searchValue: '', query: 's', page: 'Bebidas' });
+  }, [setRecipes]);
   return (
     <div>
       <Header page="Bebidas" search />
@@ -14,8 +19,12 @@ function DrinkMainPage({ match }) {
   );
 }
 
+const mapDispatchToProps = (dispatch) => ({
+  setRecipes: (search) => dispatch(searchBarFetch(search)),
+});
+
 DrinkMainPage.propTypes = {
   match: object,
 }.isRequired;
 
-export default DrinkMainPage;
+export default connect(null, mapDispatchToProps)(DrinkMainPage);
