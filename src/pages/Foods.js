5c408137c60e5340887f3c463ba9a2d5061c70b2
tiago-yoneda <xyonedax@gmail.com>
2021-04-27 10:36:26 -0300

    modificando header para acoplar o HeaderSearchBar

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,26 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../components/Header';
-import HeaderSearchBar from '../components/HeaderSearchBar';
-import searchIcon from '../images/searchIcon.svg';
 
 export default function Foods() {
-  const [searchButton, setSearchButton] = useState(false);
-
-  const handleClickSearchButton = () => {
-    setSearchButton(!searchButton);
-  };
-
   return (
     <div>
       <Header title="Comidas" />
-      <input
-        src={ searchIcon }
-        type="image"
-        data-testid="search-top-btn"
-        onClick={ handleClickSearchButton }
-        alt=""
-      />
-      {searchButton ? <HeaderSearchBar /> : null}
       <p>Esta é a pagina de comidas.</p>
     </div>
   );
