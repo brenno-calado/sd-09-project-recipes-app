1e1d5bf54894545873385c891625e444dc6debb8
Carine Becker <caarine.m@gmail.com>
2021-04-27 17:45:00 -0300

    feat: req5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,8 +4,29 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 
 function Login() {
+  const [emailIsValid, setEmailIsValid] = useState(false);
+  const [passwordIsValid, setPasswordIsValid] = useState(false);
   const [userEmail, setUserEmail] = useState('');
   const [userPassword, setUserPassword] = useState('');
+
+  // Email validation source: https://www.kindacode.com/article/live-email-validation-in-react-with-regex/
+  const validateEmail = (e) => {
+    const emailRegex = /\S+@\S+\.\S+/;
+    const email = e.target.value;
+    setUserEmail(email);
+    if (emailRegex.test(email)) {
+      return setEmailIsValid(true);
+    } setEmailIsValid(false);
+  };
+
+  const validatePassword = (e) => {
+    const password = e.target.value;
+    const minPasswordLength = 6;
+    setUserPassword(password);
+    if (password.length >= minPasswordLength) {
+      return setPasswordIsValid(true);
+    } setPasswordIsValid(false);
+  };
 
   return (
     <main>
@@ -18,7 +39,7 @@ function Login() {
               type="email"
               name="email"
               value={ userEmail }
-              onChange={ (e) => setUserEmail(e.target.value) }
+              onChange={ validateEmail }
               placeholder="Insira seu email"
               data-testid="email-input"
             />
@@ -29,7 +50,7 @@ function Login() {
               type="password"
               name="password"
               value={ userPassword }
-              onChange={ (e) => setUserPassword(e.target.value) }
+              onChange={ validatePassword }
               placeholder="Insira sua senha"
               data-testid="password-input"
             />
@@ -37,6 +58,7 @@ function Login() {
           <Button
             type="submit"
             data-testid="login-submit-btn"
+            disabled={ emailIsValid && passwordIsValid ? '' : true }
           >
             Entrar
           </Button>
