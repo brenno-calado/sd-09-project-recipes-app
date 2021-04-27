4066867d22d13fee4c039a4b80b86668673c3568
BBrolesi <brunohsbrolesi@gmail.com>
2021-04-27 19:05:07 -0300

    test: add test file and import dependencies

---
+++ src/tests/recipeMain.test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import BottomNav from '../components/BottomNav';
+import renderWithRouter from './renderWithRouter';
