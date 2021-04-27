a452b6b450008cd6b259976cb2b9fbf1360b474c
Matheus Martino <matheus.martino97@gmail.com>
2021-04-27 19:09:35 -0300

    Implementando o footer.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+function Footer() {
+  return (
+    <footer className="Footer" data-testid="footer">
+      <Link to="/bebidas">
+        <button type="button">
+          <img src={ drinkIcon } alt="drink" data-testid="drinks-bottom-btn" />
+        </button>
+      </Link>
+      <Link to="/explorar">
+        <button type="button">
+          <img
+            src={ exploreIcon }
+            alt="explore"
+            data-testid="explore-bottom-btn"
+          />
+        </button>
+      </Link>
+      <Link to="/comidas">
+        <button type="button">
+          <img src={ mealIcon } alt="drink" data-testid="food-bottom-btn" />
+        </button>
+      </Link>
+    </footer>
+  );
+}
+
+export default Footer;
