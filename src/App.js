6bd6e473945217085f3a1551b1975be6118806af
Enio Nicael <enionicael@gmail.com>
2021-04-27 00:54:10 -0300

    reset do ultimo commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+<<<<<<< HEAD
 // import rockGlass from './images/rockGlass.svg';
 import BottomMenu from './components/BottomMenu';
+=======
+>>>>>>> be5635b0e91dc7af2e459456d36146e917bf4688
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './Pages/Login';
 
 function App() {
   return (
+<<<<<<< HEAD
     <BottomMenu />
+=======
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    </BrowserRouter>
+>>>>>>> be5635b0e91dc7af2e459456d36146e917bf4688
   );
 }
 
