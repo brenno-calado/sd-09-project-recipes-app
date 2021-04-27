984e7a6afe7c64de49ac88396ec417707c2bdc43
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-04-27 15:57:37 -0400

    Grupo 24 - Requisito 27

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
