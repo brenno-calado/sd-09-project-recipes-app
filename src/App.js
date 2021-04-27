00ef67d16e86bc7bcb9ad473853bbccdca757c74
Murilo <mub4si@gmail.com>
2021-04-27 10:21:58 -0300

    Aplica .skip aos testes do cypress que nao se aplicam a esse componente

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FooterMenu from './components/FooterMenu';
+import Login from './Pages/Login';
+import Foods from './Pages/Foods';
+import Drinks from './Pages/Drinks';
+import Explore from './Pages/Explore';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <>
+      <div className="meals">
+        <span className="logo">TRYBE</span>
+        <object
+          className="rocksGlass"
+          type="image/svg+xml"
+          data={ rockGlass }
+        >
+          Glass
+        </object>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/comidas" component={ Foods } />
+          <Route path="/bebidas" component={ Drinks } />
+          <Route path="/explorar" component={ Explore } />
+        </Switch>
+      </div>
+      <FooterMenu />
+    </>
   );
 }
 
