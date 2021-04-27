da2644af4786af7b6b3851249d7cddd5ec83b4f5
Samuel <samuelvanguarda@gmail.com>
2021-04-27 18:35:25 -0300

    [FEAT][Requisito 2 ao 8][Tela de Login]

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const Login = () => (
-  <div>
-    Login
-  </div>
-);
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [validationEmail, setValidationEmail] = useState(false);
+  const [validationPassword, setValidationPassword] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+
+  const activateButton = () => {
+    if (validationEmail && validationPassword) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  };
+
+  const validateEmail = ({ target }) => {
+    const check = /^[\S.]+@[a-z]+\.\w{2,3}$/g.test(target.value);
+    setEmail(target.value);
+    setValidationEmail(check);
+    activateButton();
+  };
+
+  const validatePassword = ({ target }) => {
+    const min = 6;
+    if (target.value.length >= min) {
+      setValidationPassword(true);
+    } else {
+      setValidationPassword(false);
+    }
+    activateButton();
+  };
+
+  const saveTokens = () => {
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email }));
+  };
+
+  return (
+    <div>
+      Login
+      <input data-testid="email-input" onChange={ validateEmail } />
+      <input data-testid="password-input" onChange={ validatePassword } />
+      <Link to="/comidas">
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+          disabled={ disabled }
+          onClick={ saveTokens }
+        >
+          Entrar
+        </button>
+      </Link>
+    </div>
+
+  );
+};
 
 export default Login;
