bfcd3e427915448a72bd3cbea48afd07160781de
Daniela Muller <danielamuller20@gmail.com>
2021-04-28 10:06:06 -0300

    lint

--- src/pages/FoodRecipe.js
+++ src/pages/FoodRecipe.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { shape, string } from 'prop-types';
 
 function FoodRecipe(props) {
-  
+  const { match } = props;
+  const { params } = match;
+  const { id } = params;
   return (
-    <h1>Detalhes da receita{props.match.params.id}</h1>
+    <h1>
+      Detalhes da receita
+      { id }
+    </h1>
   );
 }
+
+FoodRecipe.propTypes = {
+  match: shape(),
+  params: shape(),
+  id: string,
+};
+
+FoodRecipe.defaultProps = {
+  match: {},
+  params: {},
+  id: '',
+};
 
 export default FoodRecipe;
