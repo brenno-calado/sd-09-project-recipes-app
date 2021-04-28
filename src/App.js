339d23930475de655f7829b6852c2f4087d1cf5b
Luciano_Lodi <lrlodi@gmail.com>
2021-04-28 14:59:59 -0300

    Correcoes de conflito App.css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Common/Components/Header';
 import Profile from './Pages/Profile';
+import MealRecipeDetails from './Pages/RecipeDetails';
+import testRecipeDetailsObj from './Common/testRecipeDetailsObj';
 
 class App extends React.Component {
   render() {
@@ -12,8 +14,12 @@ class App extends React.Component {
       <BrowserRouter>
         <div className="meals">
           <Switch>
-            <Route exact path="/" render={ Header } />
+            <Route exact path="/" render={ () => <Header pageName="Receita" /> } />
             <Route path="/perfil" component={ Profile } />
+            <Route
+              path="/mealrecipedetails"
+              render={ () => <MealRecipeDetails recipe={ testRecipeDetailsObj } /> }
+            />
           </Switch>
         </div>
       </BrowserRouter>
