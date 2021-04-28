4ee0325d6a35899d2fb44f731b1671756cd80447
BBrolesi <brunohsbrolesi@gmail.com>
2021-04-28 19:05:37 -0300

    test: Mock Context implementation

--- src/tests/recipeMain.test.js
+++ src/tests/recipeMain.test.js
@@ -1,4 +1,31 @@
 import React from 'react';
-import userEvent from '@testing-library/user-event';
-import BottomNav from '../components/BottomNav';
-import renderWithRouter from './renderWithRouter';
+import { render } from '@testing-library/react';
+import RecipeMain from '../pages/RecipeMain';
+import MealsAndDrinkContext from '../context/MealsAndDrinkContext';
+
+import meals from './mocks/mealsMocks';
+
+const renderRecipeMain = () => (render(
+  <MealsAndDrinkContext.Provider value={ meals }>
+    <RecipeMain />
+  </MealsAndDrinkContext.Provider>,
+));
+describe('Requirement 25: Test Recipe Main Page with correct data-testid',
+  () => {
+    it('Has the correct data-testids in rendered food cards', () => {
+      const { getAllByTestId } = renderRecipeMain();
+    });
+    it('Has the correct data-testids in rendered drink cards', () => {
+
+    });
+  });
+
+describe('Requirement 26: render de 12 first food or drink recipes ',
+  () => {
+    it('Has the correct data-testids in rendered food cards', () => {
+      const { getByTestId } = renderRecipeMain();
+    });
+    it('Has the correct data-testids in rendered drink cards', () => {
+
+    });
+  });
