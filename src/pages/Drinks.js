cd9371c88b5ee7db1ff248655a159b4f14d1c633
negres <nataliaknegres@gmail.com>
2021-04-28 16:23:49 -0300

    Corrige alguns estilos

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { objectOf } from 'prop-types';
 import Header from '../components/Header';
+import SearchBar from '../components/SearchBar';
+import RecipeCard from '../components/RecipeCard';
 
 class Bebidas extends React.Component {
   render() {
+    const { recipes } = this.props;
     const searchIcon = true;
     return (
-      <Header title="Bebidas" searchIcon={ searchIcon } />
+      <div>
+        <Header title="Bebidas" searchIcon={ searchIcon } />
+        <SearchBar />
+        <div className="recipe-card-container">
+          {recipes.meals && recipes.meals.map((meal) => (
+            <RecipeCard key={ meal.idMeal } meal={ meal } />))}
+        </div>
+      </div>
     );
   }
 }
+
+Bebidas.propTypes = {
+  recipes: objectOf,
+}.isRequired;
 
 export default Bebidas;
