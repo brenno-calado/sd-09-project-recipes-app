56e7d496dbfe871ad603c216734ecf562dbf1592
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2021-04-28 19:33:07 -0300

    req 13 fixed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,32 +2,31 @@ import React from 'react';
 
 export default function SearchBar() {
   return (
-    <div className="search-bar">
-      <input type="text" data-testid="search-input" />
-      <div>
-        <span>Ingrediente</span>
-        <input
-          type="radio"
-          name="filter"
-          value="ingredient"
-          data-testid="ingredient-search-radio"
-        />
-        <span>Nome</span>
-        <input
-          type="radio"
-          name="filter"
-          value="name"
-          data-testid="name-search-radio"
-        />
-        <span> Primeira letra</span>
-        <input
-          type="radio"
-          name="filter"
-          value="first-letter"
-          testid="first-letter-search-radio"
-        />
+    <div data-testid="search-top-btn">
+      <div className="search-bar">
+        <input type="text" data-testid="search-input" />
+        <div>
+          <input
+            type="radio"
+            name="filter"
+            value="ingredient"
+            data-testid="ingredient-search-radio"
+          />
+          <input
+            type="radio"
+            name="filter"
+            value="name"
+            data-testid="name-search-radio"
+          />
+          <input
+            type="radio"
+            name="filter"
+            value="first-letter"
+            data-testid="first-letter-search-radio"
+          />
+        </div>
+        <button type="button" data-testid="exec-search-btn">Buscar</button>
       </div>
-      <button type="button" data-testid="exec-search-btn">Buscar</button>
     </div>
   );
 }
