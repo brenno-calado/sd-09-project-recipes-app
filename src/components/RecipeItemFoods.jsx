5ec52dc4915dffcc7551867319017a178e74daf3
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-04-27 20:35:40 -0400

    Adiciona requisito 29 ate 32

--- src/components/RecipeItemFoods.jsx
+++ src/components/RecipeItemFoods.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { object, number } from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function RecipeItemFood({ recipe, index }) {
   return (
-    <div data-testid={ `${index}-recipe-card` }>
-      <img
-        src={ recipe.strMealThumb }
-        alt="Recipe"
-        data-testid={ `${index}-card-img` }
-      />
-      <h2 data-testid={ `${index}-card-name` }>{ recipe.strMeal }</h2>
-    </div>
+    <Link to={ `/comidas/${recipe.idMeal}` }>
+      <div data-testid={ `${index}-recipe-card` }>
+        <img
+          src={ recipe.strMealThumb }
+          alt="Recipe"
+          data-testid={ `${index}-card-img` }
+        />
+        <h2 data-testid={ `${index}-card-name` }>{ recipe.strMeal }</h2>
+      </div>
+    </Link>
   );
 }
 
