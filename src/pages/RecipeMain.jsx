a549a50af105c9ab492fb684645fc1c813faab8d
BBrolesi <brunohsbrolesi@gmail.com>
2021-04-28 19:06:33 -0300

    feature: add recipe card render

--- src/pages/RecipeMain.jsx
+++ src/pages/RecipeMain.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import RecipeCard from '../components/RecipeCard';
+import MealsAndDrinkContext from '../context/MealsAndDrinkContext';
+
+const renderCards = (recipes) => {
+  if (recipes.length === 0) {
+    return;
+  }
+  return (recipes.map((recipe, index) => (
+    <RecipeCard
+      image={ recipe.strMealThumb }
+      recipeName={ recipe.strMeal }
+      index={ index }
+      key={ index }
+    />
+  )));
+};
 
 function RecipeMain() {
+  const { meals } = useContext(MealsAndDrinkContext);
+  const [currentRecipes, setCurrentRecipes] = useState([]);
+  useEffect(() => {
+    const maxRecipes = 12;
+    const recipes = meals ? meals.slice(0, maxRecipes) : [];
+    setCurrentRecipes(recipes);
+    console.log(recipes);
+  }, [meals]);
   return (
     <div className="main-container">
-      <RecipeCard
-        image="https://images.unsplash.com/photo-1546549032-9571cd6b27df?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-        recipeName="Macarrão"
-      />
+      {renderCards(currentRecipes)}
     </div>
   );
 }
