6bb7ceb74d3f5de1e7fb344e841d5f3ef4cbbc2d
RodrigoNeoEx <draja.acc@gmail.com>
2021-04-28 15:22:19 -0300

    fixed all header reqs

---
+++ src/Pages/ Explore.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Components/Header';
+
+class Explore extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header name="Explorar" />
+      </div>
+    );
+  }
+}
+
+export default Explore;
