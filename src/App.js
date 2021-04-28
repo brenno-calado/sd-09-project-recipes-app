43f1e011c7b1d13dd4efdfe5abc8db4fc7f1235d
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-04-27 21:48:16 -0300

    Req 20

--- src/App.js
+++ src/App.js
@@ -9,21 +9,21 @@ function App() {
   return (
     <Switch>
       <Route path="/" component={ Login } />
-      {/* <Route path="/comidas" component={} />
-      <Route path="/bebidas" component={} />
-      <Route path="/comidas/:id/in-progress" component={} />
-      <Route path="/bebidas/:id/in-progress" component={} />
-      <Route path="/comidas/:id" component={} />
-      <Route path="/bebidas/:id" component={} />
-      <Route path="/explorar/comidas/ingredientes" component={} />
-      <Route path="/explorar/bebidas/ingredientes" component={} />
-      <Route path="/explorar/comidas" component={} />
-      <Route path="/explorar/bebidas" component={} />
-      <Route path="/explorar/comidas/area" component={} />
-      <Route path="/explorar" component={} />
-      <Route path="/perfil" component={} />
-      <Route path="/receitas-feitas" component={} />
-      <Route path="/receitas-favoritas" component={} /> */}
+      {/* <Route path="/comidas" component={} /> */}
+      {/* <Route path="/bebidas" component={} /> */}
+      {/* <Route path="/comidas/:id/in-progress" component={} /> */}
+      {/* <Route path="/bebidas/:id/in-progress" component={} /> */}
+      {/* <Route path="/comidas/:id" component={} /> */}
+      {/* <Route path="/bebidas/:id" component={} /> */}
+      {/* <Route path="/explorar/comidas/ingredientes" component={} /> */}
+      {/* <Route path="/explorar/bebidas/ingredientes" component={} /> */}
+      {/* <Route path="/explorar/comidas" component={} /> */}
+      {/* <Route path="/explorar/bebidas" component={} /> */}
+      {/* <Route path="/explorar/comidas/area" component={} /> */}
+      {/* <Route path="/explorar" component={} /> */}
+      {/* <Route path="/perfil" component={} /> */}
+      {/* <Route path="/receitas-feitas" component={} /> */}
+      {/* <Route path="/receitas-favoritas" component={} /> */}
     </Switch>
   );
 }
