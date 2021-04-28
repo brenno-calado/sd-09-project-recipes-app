3021d0092b2b921a25958135884edf7c795fea8a
brunamfguimaraes <brunamfguimaraes@gmail.com>
2021-04-28 18:34:53 -0300

    Add/ExplorarComidasEBebidas

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -5,6 +5,25 @@ import Header from '../components/Header';
 function ExploreDrinks() {
   return (
     <div>
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+      >
+        Por Ingredientes
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-area"
+      >
+        Por Local de Origem
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </button>
+
       <Header />
       <Footer />
     </div>
