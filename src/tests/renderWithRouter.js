d2ac625ffc77bd5e45c5f32785366d00865f0433
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-04-28 16:35:09 -0300

    Cobertura de testes da pagina Login = 100%

--- src/tests/renderWithRouter.js
+++ src/tests/renderWithRouter.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
+import store from '../store/store';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
   return {
-    ...render(<Router history={ history }>{component}</Router>), history,
+    ...render(
+      <Provider store={ store }>
+        <Router history={ history }>
+          {component}
+        </Router>
+      </Provider>,
+    ),
+    history,
   };
 };
 
