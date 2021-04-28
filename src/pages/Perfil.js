19d51b0166fe8fcc9f7b15b4166b4301e2476b4b
MarcosAlbuquerque <mpdsa20@gmail.com>
2021-04-28 17:26:20 -0300

    Tela de perfil [OK] / Tela de explorar [OK]

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -6,6 +7,31 @@ function Perfil() {
   return (
     <>
       <Header title="Perfil" searchIcon={ false } />
+      <h3 data-testid="profile-email">
+        { JSON.parse(localStorage.getItem('user')).email }
+      </h3>
+      <Link to="/receitas-feitas">
+        <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+      </Link>
+      <br />
+      <Link to="/receitas-favoritas">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+      <br />
+      <Link to="/">
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => localStorage.clear() }
+        >
+          Sair
+        </button>
+      </Link>
       <Footer />
     </>
   );
