6a82e2a1a65a847788a92b106cf7064673c28cf2
BBrolesi <brunohsbrolesi@gmail.com>
2021-04-28 19:02:50 -0300

    fix: RecipeCard data-testid

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -3,20 +3,27 @@ import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
 
 function RecipeCard(props) {
-  const { image, recipeName } = props;
+  const { image, recipeName, index } = props;
   return (
-    <Card style={ { width: '18rem' } }>
-      <Card.Img variant="top" src={ image } />
-      <Card.Body>
-        <Card.Title>{recipeName}</Card.Title>
-      </Card.Body>
-    </Card>
+    <div data-testid={ `${index}-recipe-card` }>
+      <Card style={ { width: '18rem' } }>
+        <div data-testid={ `${index}-recipe-img` }>
+          <Card.Img variant="top" src={ image } />
+        </div>
+        <Card.Body>
+          <div data-testid={ `${index}-recipe-name` }>
+            <Card.Title>{recipeName}</Card.Title>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
 RecipeCard.propTypes = {
   image: PropTypes.string.isRequired,
   recipeName: PropTypes.string.isRequired,
+  index: PropTypes.string.isRequired,
 };
 
 export default RecipeCard;
