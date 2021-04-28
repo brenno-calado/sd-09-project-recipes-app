0c4ff595b8fe642996c199fa1ab1db0634c2b530
Nícolas Tozato <ntozato9@gmail.com>
2021-04-28 17:58:35 -0300

    corrige lint

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,9 +13,30 @@ const Footer = () => {
   const history = useHistory();
   return (
     <footer data-testid="footer" style={ footerStyle }>
-      <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="drink-icon" onClick={ () => history.push('/bebidas') } />
-      <img data-testid="explore-bottom-btn" src={ exploreIcon } alt="explore-icon" onClick={ () => history.push('/explorar') } />
-      <img data-testid="food-bottom-btn" src={ mealIcon } alt="meal-icon" onClick={ () => history.push('/comidas') } />
+      <button type="button" onClick={ () => history.push('/bebidas') }>
+        <img
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt="drink-icon"
+
+        />
+      </button>
+      <button type="button" onClick={ () => history.push('/explorar') }>
+        <img
+          data-testid="explore-bottom-btn"
+          src={ exploreIcon }
+          alt="explore-icon"
+
+        />
+      </button>
+
+      <button type="button" onClick={ () => history.push('/comidas') }>
+        <img
+          data-testid="food-bottom-btn"
+          src={ mealIcon }
+          alt="meal-icon"
+        />
+      </button>
     </footer>
   );
 };
