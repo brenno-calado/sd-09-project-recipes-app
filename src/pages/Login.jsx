ba4fdc8572d764a1541504c02ed07ed0b54136f6
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-04-28 20:52:00 -0300

    Requisito 05

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,7 +20,7 @@ class Login extends Component {
 
   handlerEmail({ target: { value } }) {
     const regex = (
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+      /^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9-]+\.([a-zA-Z0-9.]{2,})+$/);
     const isvalid = regex.test(value);
 
     this.setState({
