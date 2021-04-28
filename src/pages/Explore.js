84fdbc84f93d207cbf23e46d992176c8958c9387
brunamfguimaraes <brunamfguimaraes@gmail.com>
2021-04-28 17:52:50 -0300

    Add/TelaDeExplorar

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import RecipesContext from '../context/RecipesContext';
+import Header from '../components/Header';
 
 function Explore() {
+  const history = useHistory();
+  const { pathname } = history.location;
+  const { setTitle } = useContext(RecipesContext);
+
+  const handleClickExploreFoods = () => {
+    history.push('/explorar/comidas');
+    setTitle('Explorar Comidas');
+  };
+
+  const handleClickExploreDrinks = () => {
+    history.push('/explorar/bebidas');
+    setTitle('Explorar Bebidas');
+  };
+
   return (
     <div>
-      Oi
+      <Header />
+
+      { pathname === '/explorar' ? (
+        <div>
+          <button
+            type="button"
+            data-testid="explore-food"
+            onClick={ handleClickExploreFoods }
+          >
+            Explorar Comidas
+          </button>
+          <br />
+          <br />
+          <button
+            type="button"
+            data-testid="explore-drinks"
+            onClick={ handleClickExploreDrinks }
+          >
+            Explorar Bebidas
+          </button>
+        </div>) : null }
     </div>
   );
 }
