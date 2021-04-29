5def1773f5d39caa85f7b6bc945038c2258cfb8b
Miguel Dantas <miiguelalb@gmail.com>
2021-04-29 20:43:38 -0300

    Adcionado testid dos botoes

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Header from '../components/header'
 import Footer from '../components/footer';
 
 export default function MainPageFood() {
   return (
-    <div>
-      <div>
+    <>
+      <Header page="Perfil" />
+      <main>
         <Link to="/myRecipes">
-          <h2>Receitas Feitas</h2>
+          <btn data-testid="profile-done-btn">Receitas Feitas</btn>
         </Link>
         <Link to="/favoriteRecipes">
-          <h2>Receitas Favoritas</h2>
+          <btn testid="profile-favorite-btn">Receitas Favoritas</btn>
         </Link>
         <Link to="/mainPage">
-          <h2>Sair</h2>
+          <brn data-testid="profile-logout-btn">Sair</brn>
         </Link>
-      </div>
+      </main>
       <Footer />
-    </div>
+    </>
   );
 }
