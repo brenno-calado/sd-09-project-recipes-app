5cdf81635a5331527245acd6dac69aaad24c64b1
Rafael Pufal <rafael.pufal23@gmail.com>
2021-04-29 00:12:36 -0300

    reqs 47, 48, 49, 52 e 53 mocados

---
+++ src/context/mockInfo.js
@@ -0,0 +1,14 @@
+const mockDrink = {
+  strDrink: 'Margarita',
+  strCategory: 'Ordinary Drink',
+  strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg',
+  strInstructions: 'Rub the rim of the glass with the lime slice to make the salt stick to it.Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.',
+  ingredientsAndMeasurements: [
+    { name: 'Tequila', measure: '1 1/2 oz' },
+    { name: 'Triple sec', measure: '1/2 oz' },
+    { name: 'Lime juice', measure: '1 oz' },
+    { name: 'Salt', measure: '' },
+  ],
+};
+
+export default mockDrink;
