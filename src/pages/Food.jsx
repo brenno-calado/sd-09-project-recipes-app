a54604ae073418dbb0616c86fed87fb058747a3a
Henrique-Moura <73261812+Henrique-Moura@users.noreply.github.com>
2021-04-29 13:53:31 -0300

    Update src/pages/Food.jsx
    Co-authored-by: Brenno Calado <66017138+brenno-calado@users.noreply.github.com>

--- src/pages/Food.jsx
+++ src/pages/Food.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
-// import SearchBar from '../components/SearchBar';
 
 function Food({ match }) {
   return (
