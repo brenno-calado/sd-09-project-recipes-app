464208b48b76a48d656556cba08702e405c8f0f2
danielfasanaro <danielfasanaro@gmail.com>
2021-04-29 18:21:39 -0300

    Req.67-69: Add Explore page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import DrinkDetails from './pages/DrinkDetails';
 import FoodsMainPage from './pages/FoodsMainPage';
 import DrinksMainPage from './pages/DrinksMainPage';
 import Profile from './pages/Profile';
+import Explore from './pages/Explore';
 import './App.css';
 // import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -19,6 +20,7 @@ function App() {
       <Route path="/perfil" component={ Profile } />
       <Route path="/comidas/:id" component={ MealDetails } />
       <Route path="/bebidas/:id" component={ DrinkDetails } />
+      <Route path="/explorar" component={ Explore } />
       {/* <Route path="/comidas/:id/in-progress" component={} />
       <Route path="/bebidas/:id/in-progress" component={} />
       <Route path="/explorar/comidas/ingredientes" component={} />
@@ -26,7 +28,6 @@ function App() {
       <Route path="/explorar/comidas" component={} />
       <Route path="/explorar/bebidas" component={} />
       <Route path="/explorar/comidas/area" component={} />
-      <Route path="/explorar" component={} />
       <Route path="/receitas-feitas" component={} />
       <Route path="/receitas-favoritas" component={} /> */}
     </Switch>
