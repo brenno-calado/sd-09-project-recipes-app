5d4a96a0b5ca4d75e5e6137e7f6084527bb04c6f
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-04-29 11:45:00 -0300

    contextos setados

--- src/contexts/Context.tsx
+++ src/contexts/Context.tsx
@@ -1,5 +1,7 @@
 import React, { createContext } from 'react';
-
+import { useState } from 'react';
+import { MealServiceIngredientsAPI, MealServiceNameAPI, MealServiceFirstLetterAPI } from '../services/MealRecipesAPI';
+import { BeverageServiceIngredientsAPI, BeverageServiceNameAPI, BeverageServiceFirstLetterAPI } from '../services/BeverageRecipesAPI';
 interface ContextProviderData {
   children: React.ReactNode;
 }
@@ -7,10 +9,53 @@ interface ContextProviderData {
 export const Context = createContext({});
 
 export function ContextProvider({ children }: ContextProviderData) {
+  const [ inputToSearch, setInputToSearch ] = useState('');
+  const [ definedTypeSearch, setDefinedTypeSearch ] = useState('meals');
+  const [ filterSelected, setFilterSelected ] = useState('');
+  const [ arrayOfResults, setArrayOfResults ] = useState('');
 
+  function characterLimiter() {
+    filterSelected === 'firstLetter' && inputToSearch.length > 1 ? alert('Sua busca deve conter somente 1 (um) caracter') : null;
+  }
+
+  async function searchInMealRecepies(mealToSearch:string) {
+    if (filterSelected === 'firstLetter') {
+      return setArrayOfResults(await MealServiceFirstLetterAPI(mealToSearch))
+    };
+    return filterSelected === 'name'
+      ? setArrayOfResults(await MealServiceNameAPI(mealToSearch))
+      : setArrayOfResults(await MealServiceIngredientsAPI(mealToSearch));
+  }
+
+  async function searchInBeverageRecepies(beverageToSearch:string) {
+    if (filterSelected === 'firstLetter') {
+      return setArrayOfResults(await BeverageServiceFirstLetterAPI(beverageToSearch))
+    };
+    return filterSelected === 'name'
+      ? setArrayOfResults(await BeverageServiceNameAPI(beverageToSearch))
+      : setArrayOfResults(await BeverageServiceIngredientsAPI(beverageToSearch));
+  }
+
+  function submitInputToSearch(valueToSearch:string) {
+    definedTypeSearch === 'meals'
+      ? searchInMealRecepies(valueToSearch)
+      : searchInBeverageRecepies(valueToSearch);
+  }
 
   return (
-    <Context.Provider value="">
+    <Context.Provider
+      value={{
+        inputToSearch,
+        setInputToSearch,
+        definedTypeSearch,
+        setDefinedTypeSearch,
+        filterSelected,
+        setFilterSelected,
+        characterLimiter,
+        arrayOfResults,
+        setArrayOfResults,
+        submitInputToSearch
+      }}>
       { children }
     </Context.Provider>
   );
