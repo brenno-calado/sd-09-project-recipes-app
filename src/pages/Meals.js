e9c9dcb20c0a0ca79d024923ab6c925fed6e826d
Khyrki <khyrkimitsunobi@gmail.com>
2021-04-28 22:03:19 -0300

    resolve bug

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { objectOf, string } from 'prop-types';
 import { Redirect } from 'react-router-dom';
-
-import RecipeCard from '../components/RecipeCard';
 import Header from '../components/Header';
 import RecipeMealCard from '../components/RecipeMealCard';
 
@@ -13,17 +11,13 @@ class Meals extends React.Component {
     const searchIcon = true;
     const pathName = window.location.pathname;
     const mxmItens = 12;
-    const itens = recipes.filter((_, index) => index < mxmItens);
-    const idType = (pathName === '/comidas') ? 'idMeals' : 'idDrinks';
+    const itens = recipes && recipes.filter((_, index) => index < mxmItens);
+    const idType = (pathName === '/comidas') ? 'idMeal' : 'idDrink';
     return (
       <div>
         <Header title="Comidas" searchIcon={ searchIcon } />
-        <div className="recipe-card-container">
-          {recipes.meals && recipes.meals.map((meal) => (
-            <RecipeCard key={ meal.idMeal } meal={ meal } />))}
-        </div>
         {itens.length === 1
-          && <Redirect to={ `${pathName}/${recipes[0][idType]}` } /> }
+          && <Redirect to={ `${pathName}/${itens[0][idType]}` } /> }
         {itens.map((meal, index) => (
           <RecipeMealCard key={ meal[idType] } meal={ meal } index={ index } />))}
       </div>
