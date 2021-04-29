b8a03baab64d4281a6e4535dd51d8ff24b6d487b
LeonarDev <leonardo.majevski@gmail.com>
2021-04-29 20:06:36 -0300

    Requirements 67, 68, 69: adds the Explore Page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,25 +4,35 @@ import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import '../styles/header.css';
 
 function Header({ title }) {
   const [search, setSearch] = useState(false);
 
   function renderContent() {
     return (
-      <div>
+      <header className="container-header">
         <Link to="/perfil">
-          <img src={ profileIcon } alt="icon" data-testid="profile-top-btn" />
+          <img
+            src={ profileIcon }
+            alt="icon"
+            data-testid="profile-top-btn"
+            className="header-icon"
+          />
         </Link>
-        <h1 data-testid="page-title">{ title }</h1>
-        <button type="button" onClick={ () => setSearch(!search) }>
+        <h1 data-testid="page-title" className="title">{ title }</h1>
+        <button
+          type="button"
+          onClick={ () => setSearch(!search) }
+          className="header-button"
+        >
           <img
             src={ searchIcon }
             alt="search"
             data-testid="search-top-btn"
           />
         </button>
-      </div>
+      </header>
     );
   }
 
--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header2 from '../components/Header2';
 import Footer from '../components/Footer';
+import '../styles/explore.css';
 
 function Explore() {
   return (
     <>
       <Header2 title="Explorar" />
+      <main className="container-explore">
+        <Link to="/explorar/comidas">
+          <button
+            type="button"
+            data-testid="explore-food"
+            className="explore-button"
+          >
+            Explorar Comidas
+          </button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button
+            type="button"
+            data-testid="explore-drinks"
+            className="explore-button"
+          >
+            Explorar Bebidas
+          </button>
+        </Link>
+      </main>
       <Footer />
     </>
   );
