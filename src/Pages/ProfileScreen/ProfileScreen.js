0c00c4ae15a361bea95839bf4f565a34dc778fab
MatheuzaoCarvalho <matheusvscarvalho@gmail.com>
2021-04-29 11:50:36 -0300

    [Group17 - Tasks 82-87

--- src/Pages/ProfileScreen/ProfileScreen.js
+++ src/Pages/ProfileScreen/ProfileScreen.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
 
 function ProfileScreen() {
+  const history = useHistory();
+  const userEmail = JSON.parse(localStorage.getItem('user')) || '';
+
+  const recipesDone = () => {
+    history.push('/receitas-feitas');
+  };
+
+  const recipesFavorites = () => {
+    history.push('/receitas-favoritas');
+  };
+
+  const logout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
-    <div>
+    <div className="profile-container">
       <Header title="Perfil" />
+      <div>
+        <div>
+          <span data-testid="profile-email">{userEmail.email}</span>
+        </div>
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+          onClick={ recipesDone }
+        >
+          Receitas Feitas
+        </button>
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+          onClick={ recipesFavorites }
+        >
+          Receitas Favoritas
+        </button>
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+          onClick={ logout }
+        >
+          Sair
+        </button>
+      </div>
+      <Footer />
     </div>
   );
 }
