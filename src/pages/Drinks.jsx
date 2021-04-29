258b247e690527908f1607c7db3a7eaf168938b1
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-04-29 14:30:38 -0300

    Correçao componentes drinks

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import SearchBar from '../components/SearchBar';
 import Header from '../components/Header';
 
 function Drinks({ match }) {
