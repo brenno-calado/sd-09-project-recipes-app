5c52803651ec8be0da113c65aa5f25e99e260c1d
rafaelgeronimo <rafaelgeronimo@outlook.com>
2021-04-29 14:57:37 -0300

    Requisitos 6, 7 e 8

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './styles.css';
 
-class index extends Component {
+class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -8,6 +10,7 @@ class index extends Component {
       password: '',
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange({ target }) {
@@ -15,6 +18,16 @@ class index extends Component {
     this.setState({
       [name]: target.value,
     });
+  }
+
+  handleClick() {
+    const { email } = this.state;
+    const loginData = ({ email });
+    const { history } = this.props;
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify(loginData));
+    history.push('/comidas');
   }
 
   render() {
@@ -42,9 +55,11 @@ class index extends Component {
           required
         />
         <button
+          className="login-button"
           type="button"
           data-testid="login-submit-btn"
           disabled={ !((patternEmail.test(email)) && (password.length >= minPassLength)) }
+          onClick={ this.handleClick }
         >
           Entrar
         </button>
@@ -53,4 +68,10 @@ class index extends Component {
   }
 }
 
-export default index;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default Login;
