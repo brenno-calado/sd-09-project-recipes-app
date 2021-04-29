fae0afa0a97c88055523a4c39d3755462a16f71f
LeonardoJosedaSilveira <rpgleo.js@gmail.com>
2021-04-29 01:36:43 -0300

    TELA DE EXPLORAR DETALHES RANDON SEI LA| ate 73

--- src/pages/ExploreDrinks/index.jsx
+++ src/pages/ExploreDrinks/index.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+
+function exploreDrinkIngredient() {
+  return (
+    <Link to="/explorar/bebidas/ingredientes">
+      <button type="button" data-testid="explore-by-ingredient">Por Ingredientes</button>
+    </Link>
+  );
+}
+
+function exploreDrinkSurprise() {
+  return (
+    <Link to="/explorar/comidas">
+      <button type="button" data-testid="explore-surprise">Me Surpreenda!</button>
+    </Link>
+  );
+}
 
 export default function ExploreDrinks() {
   return (
     <>
       <Header title="Explorar Bebidas" />
+      <div>
+        { exploreDrinkIngredient() }
+        { exploreDrinkSurprise() }
+      </div>
       <Footer />
     </>
   );
--- src/pages/ExploreFood/index.jsx
+++ src/pages/ExploreFood/index.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
-
 import Footer from '../../components/Footer';
+
+function exploreFoodsIngredient() {
+  return (
+    <Link to="/explorar/comidas/ingredientes">
+      <button type="button" data-testid="explore-by-ingredient">Por Ingredientes</button>
+    </Link>
+  );
+}
+
+function exploreFoodsArea() {
+  return (
+    <Link to="/explorar/comidas/area">
+      <button type="button" data-testid="explore-by-area">Por Local de Origem</button>
+    </Link>
+  );
+}
+
+function exploreFoodsSurprise() {
+  return (
+    <Link to="/explorar/comidas">
+      <button type="button" data-testid="explore-surprise">Me Surpreenda!</button>
+    </Link>
+  );
+}
 
 export default function ExploreFood() {
   return (
     <>
       <Header title="Explorar Comidas" />
+      <div>
+        { exploreFoodsIngredient() }
+        { exploreFoodsArea() }
+        { exploreFoodsSurprise() }
+      </div>
       <Footer />
     </>
   );
