ef8b68cc746375c139c06ce52f2f3f6025424f2a
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-04-29 16:27:13 -0300

    Extrutura do compomente recipeInProgress

--- src/pages/RecipeInProgress.jsx
+++ src/pages/RecipeInProgress.jsx
@@ -6,33 +6,115 @@ import blackHeartIcon from '../images/blackHeartIcon.svg';
 function RecipeInProgress() {
   const [favorite, setFavorite] = useState(false);
 
+  const [title, setTitle] = useState('Title');
+  const [ingredients, setIngredient] = useState(['Item-1', 'Item-2', 'Item-3']);
+  const [category, setCategory] = useState('Categoria');
+  const [instructions, setInstructions] = useState('');
+
   function handleClick() {
     setFavorite(!favorite);
   }
 
-  return (
-    <div>
-      <img data-testid="recipe-photo" src="#" alt="Imagem da receita" />
-      <h2 data-testid="recipe-title">Titulo</h2>
+  function rendeImage() {
+    return (<img data-testid="recipe-photo" src="#" alt="Imagem da receita" />);
+  }
+
+  function rendeTitle() {
+    return (<h2 data-testid="recipe-title">{title}</h2>);
+  }
+
+  function rendeCategory() {
+    return (<p data-testid="recipe-category">{category}</p>);
+  }
+
+  function btnShare() {
+    return (
       <button data-testid="share-btn" type="button">
         <img src={ shareIcon } alt="Compartilhar" />
       </button>
+    );
+  }
+
+  function btnFavorite() {
+    return (
       <button
-        data-testid="share-btn"
+        data-testid="favorite-btn"
         type="button"
         onClick={ handleClick }
       >
         <img src={ favorite ? blackHeartIcon : whiteHeartIcon } alt="Favoritar" />
-
       </button>
+    );
+  }
+
+  function handleCheck({ target }) {
+    console.log(target.checked);
+  }
+
+  function henderIngredients() {
+    return (
+      <>
+        <h3>Ingredients</h3>
+        { ingredients.map((ingredient, index) => (
+          <label
+            key={ ingredient }
+            htmlFor={ ingredient }
+            data-testid={ `${index}-ingredient-step` }
+            style={ { display: 'block' } }
+          >
+            <input
+              id={ ingredient }
+              type="checkbox"
+              onChange={ handleCheck }
+              value={ ingredient }
+            />
+            { ingredient }
+          </label>
+        )) }
+      </>
+    );
+  }
+
+  function henderInstructions() {
+    return (
+      <>
+        <h3>Instructions</h3>
+        <p data-testid="instructions">{instructions}</p>
+      </>
+    );
+  }
+
+  function handleFinished() {
+    console.log('#');
+  }
+
+  function btnFinished() {
+    return (
+      <button
+        data-testid="finish-recipe-btn"
+        type="button"
+        onClick={ handleFinished }
+      >
+        Finished
+      </button>
+    );
+  }
+
+  return (
+    <div>
+      { rendeImage() }
+      { rendeTitle() }
+      { rendeCategory() }
+      { btnShare() }
+      { btnFavorite() }
+      { henderIngredients() }
+      { henderInstructions() }
+      { btnFinished() }
     </div>
   );
 }
 
-// O botão de favoritar deve possuir o atributo data-testid="favorite-btn";
 // O texto da categoria deve possuir o atributo data-testid="recipe-category";
-// Os ingredientes devem possuir o atributo data-testid=${index}-ingredient-step, a verificação será feita pelo length do atributo.
-// O elemento de instruções deve possuir o atributo data-testid="instructions";
 // O botão para finalizar a receita deve possuir o atributo data-testid="finish-recipe-btn".
 
 export default RecipeInProgress;
