c6cb66aa7cb27248202f161f866f5d16159d2200
Miguel Dantas <miiguelalb@gmail.com>
2021-04-29 20:13:23 -0300

    Page perfial criada

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer from '../components/footer';
 
 export default function MainPageFood() {
-  return (<div>Profile</div>);
+  return (
+    <div>
+      <div>
+        <Link to="/myRecipes">
+          <h2>Receitas Feitas</h2>
+        </Link>
+        <Link to="/favoriteRecipes">
+          <h2>Receitas Favoritas</h2>
+        </Link>
+        <Link to="/mainPage">
+          <h2>Sair</h2>
+        </Link>
+      </div>
+      <Footer />
+    </div>
+  );
 }
