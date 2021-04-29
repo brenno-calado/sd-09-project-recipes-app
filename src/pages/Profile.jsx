66f45c040a4fee447fcf2cdc5f1976152e083613
Mathaus <mathausannibal@gmail.com>
2021-04-29 18:56:47 -0300

    adiciona tela de perfil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-function Profile() {
-  return (
-    <>
-      <Header title="Perfil" />
-      <Footer />
-    </>
-  );
+class Profile extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header title="perfil" />
+        <p data-testid="profile-email">{localStorage.getItem('user')}</p>
+        <Link to="/receitas-feitas">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Receitas Feitas
+          </button>
+        </Link>
+        <Link to="/receitas-favoritas">
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Receitas Favoritas
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ () => localStorage.clear() }
+          >
+            Sair
+          </button>
+        </Link>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default Profile;
