246619258496681cdf26116c1269c63125d5cc4d
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-04-29 17:33:24 -0400

    Adiciona pagina de receitas completas

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,11 +1,103 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import shareIcon from '../images/shareIcon.svg';
+import '../App.css';
 
 function DoneRecipes() {
+  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
+  const [copy, setCopy] = useState(false);
+  const [recipes, setRecipes] = useState(doneRecipes);
+  const [filter, setFilter] = useState('all');
+
+  const handleShare = (type, id) => {
+    navigator.clipboard.writeText(`http://localhost:3000/${type}s/${id}`);
+    setCopy(true);
+    const timeoutToCopy = 3000;
+    setTimeout(() => {
+      setCopy(false);
+    }, timeoutToCopy);
+  };
+
+  useEffect(() => {
+    const doneRecipe = JSON.parse(localStorage.getItem('doneRecipes')) || [];
+
+    const handleFilter = () => {
+      if (filter === 'all') {
+        setRecipes(doneRecipe);
+      } else {
+        setRecipes(doneRecipe.filter((recipe) => recipe.type === filter));
+      }
+    };
+    handleFilter();
+  }, [filter]);
+
+  const renderRecipes = () => (
+    recipes.map((recipe, index) => (
+      <div key={ recipe.name }>
+        <Link to={ `/${recipe.type}s/${recipe.id}` }>
+          <img
+            src={ recipe.image }
+            alt="Recipe"
+            data-testid={ `${index}-horizontal-image` }
+            className="doneImage"
+          />
+        </Link>
+        <h4
+          data-testid={ `${index}-horizontal-top-text` }
+        >
+          { `${recipe.area} - ${recipe.category} ${recipe.alcoholicOrNot}` }
+        </h4>
+        <Link to={ `/${recipe.type}s/${recipe.id}` }>
+          <h3 data-testid={ `${index}-horizontal-name` }>{ recipe.name }</h3>
+        </Link>
+        <p data-testid={ `${index}-horizontal-done-date` }>{ recipe.doneDate }</p>
+        <button
+          type="button"
+          data-testid={ `${index}-horizontal-share-btn` }
+          src={ shareIcon }
+          onClick={ () => handleShare(recipe.type, recipe.id) }
+        >
+          Compartilhar
+        </button>
+        { copy && 'Link copiado!' }
+        { recipe.tags && recipe.tags.map((tag) => (
+          <p
+            data-testid={ `${index}-${tag}-horizontal-tag` }
+            key={ tag }
+          >
+            { tag }
+          </p>
+        )) }
+      </div>
+    ))
+  );
+
   return (
     <div>
       <Header page="Receitas Feitas" />
-      DoneRecipes
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ () => setFilter('all') }
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+        onClick={ () => setFilter('comida') }
+      >
+        Comidas
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ () => setFilter('bebida') }
+      >
+        Bebidas
+      </button>
+      { renderRecipes() }
     </div>
   );
 }
--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -1,3 +1,19 @@
 export default function setLocalStorage(key, name) {
   localStorage.setItem(key, name);
 }
+
+export const handleDone = (recipe, query) => {
+  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
+  const doneRecipe = {
+    id: recipe[`id${query}`],
+    type: query === 'Meal' ? 'comida' : 'bebida',
+    area: recipe.strArea || '',
+    category: recipe.strCategory,
+    alcoholicOrNot: recipe.strAlcoholic || '',
+    name: recipe[`str${query}`],
+    image: recipe[`str${query}Thumb`],
+    doneDate: new Date().toLocaleDateString(),
+    tags: recipe.strTags ? recipe.strTags.split(',') : '',
+  };
+  localStorage.setItem('doneRecipes', JSON.stringify([...doneRecipes, doneRecipe]));
+};
