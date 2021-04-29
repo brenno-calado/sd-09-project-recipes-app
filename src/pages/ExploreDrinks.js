c49499dcace713d9aaa222f7ccba89ad7e82de28
negres <nataliaknegres@gmail.com>
2021-04-29 15:40:24 -0300

    Adiciona requisitos 70, 71, 72 e 73

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 class ExplorarBebidas extends Component {
   render() {
     const searchIcon = false;
     return (
-      <Header title="Explorar Bebidas" searchIcon={ searchIcon } />
+      <div>
+        <Header title="Explorar Bebidas" searchIcon={ searchIcon } />
+        <Link
+          to="/explorar/bebidas/ingredientes"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </Link>
+        <Link
+          to="/explorar/bebidas/:id"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </Link>
+      </div>
     );
   }
 }
--- src/pages/ExploreMeals.js
+++ src/pages/ExploreMeals.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 class ExplorarComidas extends Component {
   render() {
     const searchIcon = false;
     return (
-      <Header title="Explorar Comidas" searchIcon={ searchIcon } />
+      <div>
+        <Header title="Explorar Comidas" searchIcon={ searchIcon } />
+        <Link
+          data-testid="explore-by-ingredient"
+          to="/explorar/comidas/ingredientes"
+        >
+          Por Ingredientes
+        </Link>
+        <Link
+          data-testid="explore-by-area"
+          to="/explorar/comidas/area"
+        >
+          Por Local de Origem
+        </Link>
+        <Link
+          data-testid="explore-surprise"
+          to="/explorar/comidas/:id"
+        >
+          Me Surpreenda!
+        </Link>
+      </div>
     );
   }
 }
