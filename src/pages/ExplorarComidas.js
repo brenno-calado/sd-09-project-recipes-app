cb399219438c072bb0017b3eaa34d5d1820c417b
MarcosAlbuquerque <mpdsa20@gmail.com>
2021-04-29 17:15:36 -0300

    Requisitos do 70 a 74 [OK]

--- src/pages/ExplorarComidas.js
+++ src/pages/ExplorarComidas.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -6,6 +7,30 @@ function Explorar() {
   return (
     <>
       <Header title="Explorar Comidas" searchIcon={ false } />
+      <Link to="/explorar/comidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to="/explorar/comidas/area">
+        <button
+          type="button"
+          data-testid="explore-by-area"
+        >
+          Por Local de Origem
+        </button>
+      </Link>
+      <Link to="/comidas/52771">
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
+      </Link>
       <Footer />
     </>
   );
