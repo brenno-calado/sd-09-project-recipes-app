c9ac6e30310f1548f169ca25eb470222a16b0e59
Demistocles <demi.treze@gmail.com>
2021-04-29 18:39:50 -0300

    concluindo o footer e a pagina explorar

--- src/pages/FoodRecipes.js
+++ src/pages/FoodRecipes.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import MenuInferior from '../components/MenuInferior';
 
 const FoodRecipes = () => (
-  <Header />
+  <div>
+    <Header />
+    <MenuInferior />
+  </div>
 );
 
 export default FoodRecipes;
