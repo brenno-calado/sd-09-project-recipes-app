ba8deadc8a3b84fd7d8016bcec8a559690acbe30
Rodrigo Farias <deltadigo@gmail.com>
2021-04-29 18:40:19 -0300

    Correções

--- src/pages/Explorer/Explorer.js
+++ src/pages/Explorer/Explorer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../../components/Header/Header';
+import Header from '../../components/Header/Index';
 
 export default function Explorer() {
   return (
