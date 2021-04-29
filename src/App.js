14119bca2f7589cd519d4b84451bebf3acde46b0
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-04-29 17:32:46 -0300

    task10

--- src/App.js
+++ src/App.js
@@ -4,12 +4,35 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
 import Comidas from './pages/Comidas';
+import DetalheComidas from './pages/DetalheComidas';
+import Bebidas from './pages/Bebidas';
+import DetalheBebidas from './pages/DetalheBebidas';
+import Explorar from './pages/Explorar';
+import ExplorarBebidas from './pages/ExplorarBebidas';
+import ExplorarComidas from './pages/ExplorarComidas';
+import ExplorarIngredientes from './pages/ExplorarIngredientes';
+import ExplorarOrigem from './pages/ExplorarOrigem';
+import Perfil from './pages/Perfil';
+import ReceitasFeitas from './pages/ReceitasFeitas';
+import ReceitasFavoritas from './pages/ReceitasFavoritas';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/explorar/comidas/area" component={ ExplorarOrigem } />
+      <Route path="/explorar/bebidas/ingredientes" component={ ExplorarIngredientes } />
+      <Route path="/explorar/comidas/ingredientes" component={ ExplorarIngredientes } />
+      <Route path="/comidas/:id" component={ DetalheComidas } />
       <Route path="/comidas" component={ Comidas } />
+      <Route path="/bebidas/:id" component={ DetalheBebidas } />
+      <Route path="/bebidas" component={ Bebidas } />
+      <Route path="/explorar/bebidas" component={ ExplorarBebidas } />
+      <Route path="/explorar/comidas" component={ ExplorarComidas } />
+      <Route path="/explorar" component={ Explorar } />
+      <Route path="/perfil" component={ Perfil } />
+      <Route path="/receitas-feitas" component={ ReceitasFeitas } />
+      <Route path="/receitas-favoritas" component={ ReceitasFavoritas } />
     </Switch>
   );
 }
