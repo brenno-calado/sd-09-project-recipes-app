856faafb87562dd9bbac78161a4f3eecd4a77405
Lucas Ribeiro <martins7ribeiro@gmail.com>
2021-04-29 17:39:46 -0300

    Tela de explorar

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import getPageTitle from '../services/pageTitles';
 
-const Explore = ({ match: { path } }) => (
-  <>
-    <Header title={ getPageTitle(path) } />
-    <Footer />
-  </>
-);
+import paths from '../routes/paths';
+
+const { EXPLORE, EXPLORE_DRINK, EXPLORE_FOOD } = paths;
+
+const Explore = ({ match: { path } }) => {
+  const history = useHistory();
+  return (
+    <>
+      <Header title={ getPageTitle(path) } />
+      {
+        path.includes(EXPLORE) && (
+          <>
+            <button
+              type="button"
+              data-testid="explore-food"
+              onClick={ () => history.push(EXPLORE_FOOD) }
+            >
+              Explorar Comidas
+            </button>
+            <button
+              type="button"
+              data-testid="explore-drinks"
+              onClick={ () => history.push(EXPLORE_DRINK) }
+            >
+              Explorar Bebidas
+            </button>
+          </>
+        )
+      }
+      <Footer />
+    </>
+  );
+};
 
 export default Explore;
 
