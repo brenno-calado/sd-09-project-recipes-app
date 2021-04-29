83cf69e48ceec4e8ca0a56cba6937a03fb3f8fa2
Rafa Reis <rafaelrnascimento2006@gmail.com>
2021-04-29 20:36:43 -0300

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { Context } from '../context/Context';
 
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
