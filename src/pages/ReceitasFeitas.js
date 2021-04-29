7f686b337dd4dc0759905ba1555f762de556106a
Lucas Silva <martssilvapro@gmail.com>
2021-04-29 16:17:58 -0300

    card mocado

--- src/pages/ReceitasFeitas.js
+++ src/pages/ReceitasFeitas.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React /* { useState } */ from 'react';
 import Header from '../components/Header';
+import shareIcon from '../images/shareIcon.svg';
 
 function ReceitasFeitas() {
   const index = '';
   const tagName = '';
+
+  const doneRecipe = {
+    type: 'Food',
+    img: 'http://www.dulcerestaurantecolonial.com.br/wp-content/uploads/2020/03/Histo%CC%81ria-da-feijoada-dulce-restaurante.jpg',
+    category: 'Food',
+    recipeName: 'Feijoada',
+    date: '10/10/10',
+    tag1: 'bun',
+    tag2: 'baking',
+    link: 'www.blbala',
+  };
+
+  const recipeArr = [];
+
+  recipeArr.push(doneRecipe);
+  localStorage.setItem('recipe', JSON.stringify(recipeArr));
+  const localData = JSON.parse(localStorage.getItem('recipe'));
+
+  // const [button, setButton] = useState();
+
   return (
     <div>
       <Header title="Receitas Feitas" searchIcon={ false } />
@@ -12,28 +33,39 @@ function ReceitasFeitas() {
         <button type="button" data-testid="filter-by-food-btn">Food</button>
         <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
       </section>
-      <section>
-        <div>
-          <div>
-            <img src="" alt="" data-testid={ `${index}-horizontal-image` } />
-          </div>
-          <h4 data-testid={ `${index}-horizontal-top-text` }>Categoria</h4>
-          <h1 data-testid={ `${index}-horizontal-name` }>
-            Nome da receita
-          </h1>
-          <p data-testid={ `${index}-horizontal-done-date` }>
-            Data da receita feita
-          </p>
-          <button
-            type="button"
-            data-testid={ `${index}-horizontal-share-btn` }
-          >
-            Compartilhar
-          </button>
-          <p data-testid={ `${index}-${tagName}-horizontal-tag` }>Tag</p>
-        </div>
-      </section>
+      {
+        localData.map((recipe) => {
+          return (
+            <div key="1">
+              <div>
+                <img
+                  src={ recipe.img }
+                  alt=""
+                  data-testid={ `${index}-horizontal-image` }
+                />
+              </div>
+              <h4 data-testid={ `${index}-horizontal-top-text` }>{ recipe.category }</h4>
+              <h1 data-testid={ `${index}-horizontal-name` }>
+                { recipe.recipeName }
+              </h1>
+              <p data-testid={ `${index}-horizontal-done-date` }>
+                { recipe.date }
+              </p>
+              <button
+                type="button"
+                onClick={ () => navigator.clipboard.writeText(recipe.link) }
+                data-testid={ `${index}-horizontal-share-btn` }
+              >
+                <img src={ shareIcon } alt="compartilhar" />
+              </button>
+              <p data-testid={ `${index}-${tagName}-horizontal-tag` }>{ recipe.tag1 }</p>
+              <p data-testid={ `${index}-${tagName}-horizontal-tag` }>{ recipe.tag2 }</p>
+            </div>
+          );
+        })
+      }
     </div>
+
   );
 }
 
