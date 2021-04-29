364f9420047fe405ae7d02749019f3e77b87025d
jiarguello <jhonatan.arguello@gmail.com>
2021-04-29 09:44:10 -0300

    Entrega dos requisitos 25 e 26.

--- src/pages/Drinks/index.jsx
+++ src/pages/Drinks/index.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { context } from '../../context';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import ListCards from '../../components/ListDrinkCards';
-import useDrinkApi from '../../hooks/useDrinkApi';
+import fetchApi from '../../services';
 
 export default function Drinks() {
-  const { drinks, setFilter } = useDrinkApi();
+  const { drinks, setDrinks, setFilter } = useContext(context);
+
+  useEffect(() => {
+    const lengthOfList = 12;
+    fetchApi('cocktail', 'name', '').then((res) => {
+      const fetchDrinks = res.drinks
+        .filter((drink) => res.drinks.indexOf(drink) < lengthOfList);
+      setDrinks(fetchDrinks);
+    });
+  }, []);
+
   return (
     <>
       <Header title="Bebidas" canFind setFilter={ setFilter } />
--- src/pages/Foods/index.jsx
+++ src/pages/Foods/index.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { context } from '../../context';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-import useFoodApi from '../../hooks/useFoodApi';
+import fetchApi from '../../services';
 import ListCards from '../../components/ListFoodCards ';
 
 export default function Foods() {
-  const { foods, setFilter } = useFoodApi();
-  console.log(foods);
+  const { foods, setFoods, setFilter } = useContext(context);
+
+  useEffect(() => {
+    const lengthOfList = 12;
+    fetchApi('food', 'name', '').then((res) => {
+      const fetchFoods = res.meals
+        .filter((food) => res.meals.indexOf(food) < lengthOfList);
+      setFoods(fetchFoods);
+    });
+  }, []);
+
   return (
     <>
       <Header title="Comidas" canFind setFilter={ setFilter } />
