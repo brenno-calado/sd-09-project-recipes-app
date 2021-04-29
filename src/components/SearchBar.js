67d5f97007dd3bb600aea24e7432d8581909f84e
richardfazeredo <richardcompletetour@gmail.com>
2021-04-28 21:33:21 -0300

    Requisitos 13 e 14

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import getFilteredRecipes from '../services/api';
 
 function SearchBar() {
+  const [searchState, setSearchState] = useState({ search: '', radioButton: '' });
+
+  const handleChange = ({ target: { value } }) => {
+    setSearchState({ ...searchState, search: value });
+  };
+
+  const handleClick = ({ target: { name } }) => {
+    setSearchState({ ...searchState, radioButton: name });
+  };
+
+  const handleSubmit = () => {
+    const { search, radioButton } = searchState;
+    console.log(getFilteredRecipes(search, radioButton));
+  };
+
   return (
     <div>
-      <input
-        data-testid="search-input"
-      />
+      <label htmlFor="search">
+        <input
+          data-testid="search-input"
+          name="search"
+          id="search"
+          onChange={ handleChange }
+        />
+      </label>
+
+      <label htmlFor="ingredient">
+        <input
+          type="radio"
+          data-testid="ingredient-search-radio"
+          name="ingrediente"
+          onClick={ handleClick }
+        />
+        Ingrediente
+      </label>
+
+      <label htmlFor="ingredient">
+        <input
+          type="radio"
+          data-testid="name-search-radio"
+          name="nome"
+          onClick={ handleClick }
+        />
+        Nome
+      </label>
+
+      <label htmlFor="ingredient">
+        <input
+          type="radio"
+          data-testid="first-letter-search-radio"
+          name="primeira-letra"
+          onClick={ handleClick }
+        />
+        Primeira letra
+      </label>
+
+      <button
+        data-testid="exec-search-btn"
+        type="button"
+        onClick={ () => handleSubmit() }
+      >
+        Buscar
+      </button>
     </div>
   );
 }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,3 @@
 export default function setStorage(key, value) {
-  // if (typeof value === 'object') {
   localStorage.setItem(key, JSON.stringify(value));
-  // } else {
-  //   localStorage.setItem(key, value);
-  // }
 }
