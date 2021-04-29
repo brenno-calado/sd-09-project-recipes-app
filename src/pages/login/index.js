683ea64d3cb533830b62126011922c24a7ad6ca7
rafaelgeronimo <rafaelgeronimo@outlook.com>
2021-04-29 14:28:58 -0300

    requisito 2

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -5,6 +5,22 @@ class index extends Component {
     return (
       <div>
         Login
+        <input
+          type="email"
+          placeholder="Email"
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          data-testid="password-input"
+        />
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+        >
+          Entrar
+        </button>
       </div>
     );
   }
