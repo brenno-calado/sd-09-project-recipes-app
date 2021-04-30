25b77d9080eb478ed9a2f8fc83abae6431f68cd4
Miguel Dantas <miiguelalb@gmail.com>
2021-04-30 15:59:29 -0300

    Todos os requisitos do perfil concluído (Falta CSS e Tests

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import Header from '../components/header';
 import Footer from '../components/footer';
 
-export default function MainPageFood() {
+export default function Perfil() {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const clearLocalStorage = () => {
+    localStorage.clear()
+  }
+
   return (
     <>
       <Header page="Perfil" />
+      <h1 data-testid="profile-email">{ user?.email }</h1>
       <main className="perfil">
         <Link to="/receitas-feitas">
           <btn data-testid="profile-done-btn">Receitas Feitas</btn>
         </Link>
         <Link to="/receitas-favoritas">
-          <btn testid="profile-favorite-btn">Receitas Favoritas</btn>
+          <btn data-testid="profile-favorite-btn">Receitas Favoritas</btn>
         </Link>
         <Link to="/">
-          <brn data-testid="profile-logout-btn">Sair</brn>
+          <brn
+            data-testid="profile-logout-btn"
+            onClick={ clearLocalStorage }
+          >
+            Sair
+          </brn>
         </Link>
       </main>
       <Footer />
