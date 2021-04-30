51fb04e8d9e680cffdf8c9b17e3edbbc5eac8d3e
Demistocles <demi.treze@gmail.com>
2021-04-30 15:58:22 -0300

    corrigindo erro de lint ¬¬

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -12,41 +12,41 @@ const Profile = () => {
     );
   }
 
-    return (
-      <div>
-        <Header />
-        <div data-testid="profile-email">
-          { email }
-        </div>
-        <div>
-          <Link to="/receitas-favoritas">
-            <button
-              data-testid="profile-favorite-btn"
-              type="button"
-            >
-              Receitas Favoritas
-            </button>
-          </Link>
-          <Link to="/receitas-feitas">
-            <button
-              data-testid="profile-done-btn"
-              type="button"
-            >
-              Receitas Feitas
-            </button>
-          </Link>
-          <Link to="/">
-            <button
-              data-testid="profile-logout-btn"
-              type="button"
-              onClick={ clearLocalStorage }
-            >
-              Sair
-            </button>
-          </Link>
-        </div>
-        <MenuInferior />
+  return (
+    <div>
+      <Header />
+      <div data-testid="profile-email">
+        { email }
       </div>
+      <div>
+        <Link to="/receitas-favoritas">
+          <button
+            data-testid="profile-favorite-btn"
+            type="button"
+          >
+            Receitas Favoritas
+          </button>
+        </Link>
+        <Link to="/receitas-feitas">
+          <button
+            data-testid="profile-done-btn"
+            type="button"
+          >
+            Receitas Feitas
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            data-testid="profile-logout-btn"
+            type="button"
+            onClick={ clearLocalStorage }
+          >
+            Sair
+          </button>
+        </Link>
+      </div>
+      <MenuInferior />
+    </div>
   );
 };
 
