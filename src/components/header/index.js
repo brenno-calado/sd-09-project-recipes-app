4e324c96c7276b98fd27e6ca7a8487e244721e13
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-04-30 12:00:19 -0300

    requirement 13,14,15,16,17,18

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.css';
 
-function index({ title }) {
+import profileIcon from '../../images/profileIcon.svg';
+import searchIcon from '../../images/searchIcon.svg';
+
+function index({ title, setSearch }) {
   return (
     <div className="container-header">
       <Link to="/perfil" data-testid="profile-top-btn">
-        <img src="profileIcon.svg" alt="profileIcon" />
+        <img src={ profileIcon } alt="profileIcon" />
       </Link>
       <h2 data-testid="page-title">{ title }</h2>
-      <button className="btn-search" type="button" data-testid="search-top-btn">
-        <img src="searchIcon.svg" alt="searchIcon" />
+      <button
+        className="btn-search-header"
+        type="button"
+        data-testid="search-top-btn"
+        onClick={ () => setSearch() }
+      >
+        <img src={ searchIcon } alt="searchIcon" />
       </button>
     </div>
   );
