ad70660ab90a4f5f09c22ec5d1d1eee9d8b3dd72
Carine Becker <caarine.m@gmail.com>
2021-04-30 19:33:11 -0300

    feat: ingredients in progress in progress

---
+++ src/components/IngredientsInProgress.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+function IngredientsInProgress() {
+  return (
+    <p>a</p>
+  );
+}
+
+export default IngredientsInProgress;
