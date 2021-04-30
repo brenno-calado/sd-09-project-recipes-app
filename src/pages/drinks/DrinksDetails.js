6df53519eb79ca00f321207f114f660a971179d2
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-04-30 18:14:23 -0300

    Tela de receitas feitas

--- src/pages/drinks/DrinksDetails.js
+++ src/pages/drinks/DrinksDetails.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Redirect } from 'react-router-dom';
 import { fetchRecipeDetails } from '../../services/api';
 import { MealsRecomendations } from '../../components';
+import shareIcon from '../../images/shareIcon.svg';
+import whiteHeartIcon from '../../images/whiteHeartIcon.svg';
+// import blackHeartIcon from '../../images/blackHeartIcon.svg';
 
 function DrinksDetails() {
   const { id } = useParams();
   const [data, setData] = useState([]);
+  const [shouldRedirect, setShouldRedirect] = useState(false);
 
   useEffect(() => {
     const getData = async () => setData(await fetchRecipeDetails(id, false));
@@ -16,23 +20,29 @@ function DrinksDetails() {
   const measures = Object.keys(data).filter((el) => el.includes('strMeasure'));
   const { strDrinkThumb, strDrink, strInstructions, strAlcoholic } = data;
 
+  if (shouldRedirect) return <Redirect to={ `/bebidas/${id}/in-progress` } />;
+
   return (
     <section className="recipe-details">
       <img data-testid="recipe-photo" src={ strDrinkThumb } alt="recipe" />
       <h2 data-testid="recipe-title">{strDrink}</h2>
-      <button data-testid="share-btn" type="button">Compartilhar</button>
-      <button data-testid="favorite-btn" type="button">Favoritar</button>
+      <button data-testid="share-btn" type="button">
+        <img src={ shareIcon } alt="share icon" />
+      </button>
+      <button data-testid="favorite-btn" type="button">
+        <img src={ whiteHeartIcon } alt="favorite icon" />
+      </button>
       <p data-testid="recipe-category">{ strAlcoholic }</p>
       { ingredients.map((ingredient, index) => (
         <p data-testid={ `${index}-ingredient-name-and-measure` } key={ ingredient }>
           { data[ingredient] && `${data[ingredient]} ${data[measures[index]]}` }
-          <input data-testid="ingredient-step" type="checkbox" />
         </p>)) }
       <p data-testid="instructions">{strInstructions}</p>
       <button
         data-testid="start-recipe-btn"
         type="button"
         className="btn-initial"
+        onClick={ () => setShouldRedirect(true) }
       >
         Iniciar Receita
       </button>
--- src/pages/meals/MealsDetails.js
+++ src/pages/meals/MealsDetails.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Redirect } from 'react-router-dom';
 import { fetchRecipeDetails } from '../../services/api';
 import { DrinksRecomendations, YoutubePlayer } from '../../components';
+import shareIcon from '../../images/shareIcon.svg';
+import whiteHeartIcon from '../../images/whiteHeartIcon.svg';
+// import blackHeartIcon from '../../images/blackHeartIcon.svg';
 
 function MealsDetails() {
   const { id } = useParams();
   const [data, setData] = useState([]);
+  const [shouldRedirect, setShouldRedirect] = useState(false);
 
   useEffect(() => {
     const getData = async () => setData(await fetchRecipeDetails(id, true));
@@ -16,26 +20,30 @@ function MealsDetails() {
   const measures = Object.keys(data).filter((el) => el.includes('strMeasure'));
   const { strMealThumb, strMeal, strCategory, strInstructions, strYoutube } = data;
 
+  if (shouldRedirect) return <Redirect to={ `/comidas/${id}/in-progress` } />;
+
   return (
     <section className="recipe-details">
       <img data-testid="recipe-photo" src={ strMealThumb } alt="recipe" />
       <h2 data-testid="recipe-title">{strMeal}</h2>
-      <button data-testid="share-btn" type="button">Compartilhar</button>
-      <button data-testid="favorite-btn" type="button">Favoritar</button>
+      <button data-testid="share-btn" type="button">
+        <img src={ shareIcon } alt="share icon" />
+      </button>
+      <button data-testid="favorite-btn" type="button">
+        <img src={ whiteHeartIcon } alt="favorite icon" />
+      </button>
       <p data-testid="recipe-category">{ strCategory }</p>
       { ingredients.map((ingredient, index) => (
         <p data-testid={ `${index}-ingredient-name-and-measure` } key={ ingredient }>
           { `${data[ingredient]} ${data[measures[index]]}` }
-          <input data-testid="ingredient-step" type="checkbox" />
         </p>)) }
       <p data-testid="instructions">{strInstructions}</p>
       <YoutubePlayer url={ strYoutube } title={ strMeal } />
-      <span data-testid="0-recomendation-card">Card de Recomendaçoes</span>
-      <span data-testid="1-recomendation-card">Card de Recomendaçoes</span>
       <button
         data-testid="start-recipe-btn"
         type="button"
         className="btn-initial"
+        onClick={ () => setShouldRedirect(true) }
       >
         Iniciar Receita
       </button>
