c817d3ea1c3f8feff81e6682f7d7b4a16eb14ca8
BernardoFontes <bfontes.1997@gmail.com>
2021-04-30 15:26:08 -0300

    req 70 a 73

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header2 from '../components/Header2';
 import Footer from '../components/Footer';
 
 function ExploreDrinks() {
   return (
     <>
-      <Footer />
       <Header2 title="Explorar Bebidas" />
+      <Link to="/explorar/bebidas/ingredientes">
+        <button
+          type="button"
+          name="Por Ingredientes"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <button
+        type="button"
+        name="Me Surpreenda!"
+        data-testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </button>
+      <Footer />
     </>
   );
 }
--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header2 from '../components/Header2';
 import Footer from '../components/Footer';
 
@@ -6,6 +7,31 @@ function ExploreFoods() {
   return (
     <>
       <Header2 title="Explorar Comidas" />
+      <Link to="/explorar/comidas/ingredientes">
+        <button
+          type="button"
+          name="Por Ingredientes"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to="/explorar/comidas/area">
+        <button
+          type="button"
+          name="Por Local de Origem"
+          data-testid="explore-by-area"
+        >
+          Por Local de Origem
+        </button>
+        <button
+          type="button"
+          name="Me Surpreenda!"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
+      </Link>
       <Footer />
     </>
   );
