79b810027c1a0468de863fe38490518bbc85d544
Renato Feitosa <pythaqua@gmail.com>
2021-04-30 17:37:12 -0300

    Add redux provider to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import store from './redux/store';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './routes';
 
 function App() {
   return (
-    <Router>
-      <Routes />
-    </Router>
+    <Provider store={ store }>
+      <Router>
+        <Routes />
+      </Router>
+    </Provider>
   );
 }
 
