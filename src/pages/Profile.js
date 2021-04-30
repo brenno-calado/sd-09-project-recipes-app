11d05d7d2faefebd606d518b93a24afc60a42fd6
Demistocles <demi.treze@gmail.com>
2021-04-30 14:59:38 -0300

    Concluindo a tela de perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import MenuInferior from '../components/MenuInferior';
+
+const { email } = JSON.parse(localStorage.getItem('user'));
+
+function clearLocalStorage() {
+  return (
+    localStorage.clear()
+  );
+}
 
 const Profile = () => (
   <div>
     <Header />
+    <div data-testid="profile-email">
+      { email }
+    </div>
+    <div>
+    <Link to="/receitas-favoritas">
+        <button
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/receitas-feitas">
+        <button
+          data-testid="profile-done-btn"
+        >
+          Receitas Feitas
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          data-testid="profile-logout-btn"
+          onClick={ clearLocalStorage }
+        >
+          Sair
+        </button>
+      </Link>
+    </div>
     <MenuInferior />
   </div>
 );
