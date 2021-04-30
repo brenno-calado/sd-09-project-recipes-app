22d3057f9391ee2195b12a1c59325ec7f008454a
diego-vini <analistamm@gmail.com>
2021-04-30 15:23:18 -0300

    Tela explorar

---
+++ src/Pages/Explorar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Footer from '../components/Footer';
+
+function Explorar() {
+  return (
+    <div className="">
+      <Link to="/explorar/comidas">
+        <button
+          className=""
+          data-testid="explore-food"
+          type="button"
+        >
+          Explorar Comidas
+        </button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button
+          className=""
+          data-testid="explore-drinks"
+          type="button"
+        >
+          Explorar Bebidas
+        </button>
+      </Link>
+      <Footer />
+    </div>
+  );
+}
+
+export default Explorar;
