0dadfacf15184e1ca3b041acc6d1d4ba6203ef04
Pedro Henrique <pedro.876.ph@gmail.com>
2021-05-01 15:34:36 -0300

    carousel passando

--- src/components/RecomendationCards.jsx
+++ src/components/RecomendationCards.jsx
@@ -1,9 +1,30 @@
 import React, { useContext, useEffect } from 'react';
+import Carousel from 'react-multi-carousel';
 import PropTypes from 'prop-types';
 import RecipesContext from '../contexts/RecipesContext';
 import fetchRecipeDetails from '../services/fetchRecipeDetails';
+import 'react-multi-carousel/lib/styles.css';
 
 const SIX = 6;
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 2,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 2,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 2,
+  },
+};
 
 function RecomendationCards({ path }) {
   const { recomendations, setRecomendations } = useContext(RecipesContext);
@@ -29,25 +50,27 @@ function RecomendationCards({ path }) {
         index < SIX && recomendation)));
 
     return (
-      recomendationsList.map((recomendation, index) => (
-        <div
-          data-testid={ `${index}-recomendation-card` }
-          className="card align-items-center m-2 flex-wrap"
-          key={ recomendation.idMeal || recomendation.idDrink }
-        >
-          <img
-            className="d-block w-100"
-            src={ recomendation.strMealThumb || recomendation.strDrinkThumb }
-            alt={ recomendation.srtMeal || recomendation.strDrink }
-          />
-          <span
-            data-testid={ `${index}-recomendation-title` }
-            className="text-content"
+      <Carousel responsive={ responsive }>
+        {recomendationsList.map((recomendation, index) => (
+          <div
+            data-testid={ `${index}-recomendation-card` }
+            className="card align-items-center m-2 flex-wrap"
+            key={ recomendation.idMeal || recomendation.idDrink }
           >
-            {recomendation.strMeal || recomendation.strDrink}
-          </span>
-        </div>
-      )));
+            <img
+              className="d-block w-100"
+              src={ recomendation.strMealThumb || recomendation.strDrinkThumb }
+              alt={ recomendation.srtMeal || recomendation.strDrink }
+            />
+            <span
+              data-testid={ `${index}-recomendation-title` }
+              className="text-content"
+            >
+              {recomendation.strMeal || recomendation.strDrink}
+            </span>
+          </div>))}
+      </Carousel>
+    );
   };
 
   return renderRecomendations();
