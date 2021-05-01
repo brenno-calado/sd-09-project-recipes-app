a4d669480cbcdf9e531d61f61a74f451c799575d
Micaela Alves <micaela.alves93@gmail.com>
2021-04-30 21:12:25 -0300

    Requisto 29 e 30

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,26 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import '../styles/Card.css';
 import { useLocation } from 'react-router-dom';
 import { RecipesContext } from '../context';
 import useRecipes from '../hooks/useRecipes';
+import getAllRecipes from '../services/allRecipesAPI';
 
 function Categories() {
   const CINCO = 5;
   const { values: { categories },
     actions: { setRecipesResult } } = useContext(RecipesContext);
-
   const { getRecipes } = useRecipes();
   const { pathname } = useLocation();
+  const elementRef = useRef(null);
 
   const filterCategory = async (strCategory) => {
     const category = pathname.includes('comidas') ? 'comidas' : 'bebidas';
-    const recipes = await getRecipes(category, strCategory, 'category');
-    if (!recipes) {
-      alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
-    } else if (!Array.isArray(recipes)) {
-      alert(recipes.message);
+    if (strCategory !== elementRef.current) {
+      const recipes = await getRecipes(category, strCategory, 'category');
+      if (!recipes) {
+        alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
+      } else if (!Array.isArray(recipes)) {
+        alert(recipes.message);
+      } else {
+        setRecipesResult(recipes);
+      }
+      elementRef.current = strCategory;
     } else {
-      setRecipesResult(recipes);
+      const allRecipes = await getAllRecipes(category);
+      setRecipesResult(allRecipes);
     }
   };
 
@@ -32,7 +39,7 @@ function Categories() {
           <button
             data-testid={ `${strCategory}-category-filter` }
             key={ `strCategory${index}` }
-            onClick={ () => filterCategory(strCategory) }
+            onClick={ () => { filterCategory(strCategory); } }
             type="button"
           >
             { strCategory }
