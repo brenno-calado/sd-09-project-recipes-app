3f382b1fbd097d81576916ed06309f7be5083816
Pedro Henrique <pedro.876.ph@gmail.com>
2021-05-02 14:37:58 -0300

    requisitos 70 a 73 feitos

--- src/pages/ExplorarBebidas.jsx
+++ src/pages/ExplorarBebidas.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Header.css';
 import Header from '../components/Header';
 import BottomMenu from '../components/BottomMenu';
 
 function ExplorarBebidas() {
+  const renderButtons = () => (
+    <>
+      <Link to="/explorar/bebidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </button>
+    </>
+  );
   return (
     <div>
       <Header page="Explorar Bebidas" hasSearchButton={ false } />
+      {renderButtons()}
       <BottomMenu />
     </div>
   );
--- src/pages/ExplorarComidas.jsx
+++ src/pages/ExplorarComidas.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Header.css';
 import Header from '../components/Header';
 import BottomMenu from '../components/BottomMenu';
 
 function ExplorarComidas() {
+  const renderButtons = () => (
+    <>
+      <Link to="/explorar/comidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to="/explorar/comidas/area">
+        <button
+          type="button"
+          data-testid="explore-by-area"
+        >
+          Por Local de Origem
+        </button>
+      </Link>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </button>
+    </>
+  );
+
   return (
     <div>
       <Header page="Explorar Comidas" hasSearchButton={ false } />
+      {renderButtons()}
       <BottomMenu />
     </div>
   );
