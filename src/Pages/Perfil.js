fe24e7747b76ee5fa3af1b65d265c8a6e9cbfc47
Vinicius Chagas <viniciushrcs@gmail.com>
2021-05-02 13:00:29 -0300

    corrigindo issues no lint

--- src/Pages/Perfil.js
+++ src/Pages/Perfil.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import MenuInferior from '../components/MenuInferior'
+import MenuInferior from '../components/MenuInferior';
 
 export default function Perfil() {
   const { email } = JSON.parse(localStorage.getItem('user'));
--- src/components/MenuInferior.js
+++ src/components/MenuInferior.js
@@ -4,30 +4,30 @@ import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
-export default function() {
-  return(
+export default function () {
+  return (
     <div data-testid="footer" className="footer">
-    <Link to="/bebidas">
-      <img
-        data-testid="drinks-bottom-btn"
-        src={ drinkIcon }
-        alt="drinkIcon"
-      />
-    </Link>
-    <Link to="/explorar">
-      <img
-        data-testid="explore-bottom-btn"
-        src={ exploreIcon }
-        alt="explorerIcon"
-      />
-    </Link>
-    <Link to="/comidas">
-      <img
-        data-testid="food-bottom-btn"
-        src={ mealIcon }
-        alt="mealIcon"
-      />
-    </Link>
-  </div>
-  )
+      <Link to="/bebidas">
+        <img
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt="drinkIcon"
+        />
+      </Link>
+      <Link to="/explorar">
+        <img
+          data-testid="explore-bottom-btn"
+          src={ exploreIcon }
+          alt="explorerIcon"
+        />
+      </Link>
+      <Link to="/comidas">
+        <img
+          data-testid="food-bottom-btn"
+          src={ mealIcon }
+          alt="mealIcon"
+        />
+      </Link>
+    </div>
+  );
 }
