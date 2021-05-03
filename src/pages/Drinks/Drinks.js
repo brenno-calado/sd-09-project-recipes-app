f46af7f9b601a27221a6fd35e030475e37a090bf
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-05-03 18:06:19 -0300

    Pagina Principal de comidas e bebidas [req 35 ao 32]

--- src/pages/Drinks/Drinks.js
+++ src/pages/Drinks/Drinks.js
@@ -1,13 +1,111 @@
-import React from 'react';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-export default function Drinks() {
-  return (
+import PropTypes from 'prop-types';
+import Header from '../../components/Header';
+import Card from '../../components/Card';
+import { DrinkCtx } from '../../context/contextDrink';
+import Footer from '../../components/Footer';
+import CategoryBtn from '../../components/Buttons/CategoryBtn';
+
+function Drinks({ location: { state } }) {
+  const STOP_INDEX = 11;
+  const { drinkApi: { drinks }, setFilterDrink } = useContext(DrinkCtx);
+  const [category, setCategory] = useState('');
+  const [ingredient, setIngredient] = useState('');
+  const history = useHistory();
+  const onClickAll = ({ target }) => {
+    if (state && state.fromExplorerDrinksIngredients) {
+      state.fromExplorerDrinksIngredients = false;
+    }
+    setCategory(target.value);
+    setFilterDrink({ key: 'name', value: category });
+  };
+  const onClickCategory = ({ target }) => {
+    if (category !== target.value) {
+      setCategory(target.value);
+      if (state && state.fromExplorerDrinksIngredients) {
+        state.fromExplorerDrinksIngredients = false;
+      }
+    } else { setCategory(''); }
+  };
+
+  useEffect(() => {
+    if (state !== undefined && state.fromExplorerDrinksIngredients) {
+      setIngredient(state.ingredient);
+      setFilterDrink({ key: 'ing', value: ingredient });
+    } else {
+      setFilterDrink({ key: 'category', value: category });
+    }
+  }, [category, setFilterDrink, state, ingredient]);
+
+  useEffect(() => {
+    const categorySetting = (categoryState) => {
+      if (categoryState === '' && state === undefined) {
+        setFilterDrink({ key: 'name', value: categoryState });
+      }
+    }; categorySetting(category);
+  }, [category, setFilterDrink, state]);
+
+  const render = () => (
     <div>
-      <Header title="Bebidas" icon="true" currentPage="Foods" />
-      Drinks
+      <Header name="Bebidas" icon="true" currentPage="Drinks" />
+      <CategoryBtn
+        label="Drinks"
+        onClickAll={ onClickAll }
+        onClickCategory={ onClickCategory }
+      />
+      <div className="cards ingredient-card">
+        {drinks && drinks
+          .filter((drink, index) => index <= STOP_INDEX)
+          .map((item, index) => (
+            <Card
+              key={ item.idDrink }
+              id={ item.idDrink }
+              name={ item.strDrink }
+              img={ item.strDrinkThumb }
+              index={ index }
+              onClick={ () => history.push(`/bebidas/${item.idDrink}`) }
+            />
+          ))}
+        {console.log(state)}
+        { (drinks && drinks.length === 1 && category === '')
+          ? history.push(`/bebidas/${drinks[0].idDrink}`)
+          : ''}
+        { drinks === null
+          // eslint-disable-next-line no-alert
+          ? alert('Sinto muito, não encontramos nenhuma receita para esses filtros.')
+          : ''}
+      </div>
+      <div className="spacing" />
       <Footer />
     </div>
   );
+
+  return (render());
 }
+
+Drinks.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string,
+    search: PropTypes.string,
+    hash: PropTypes.string,
+    state: PropTypes.shape({
+      fromExplorerDrinksIngredients: PropTypes.bool,
+      ingredient: PropTypes.string,
+    }),
+    key: PropTypes.string,
+  }),
+};
+
+Drinks.defaultProps = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string,
+    search: PropTypes.string,
+    hash: PropTypes.string,
+    state: PropTypes.string,
+    key: PropTypes.string,
+  }),
+};
+
+export default Drinks;
--- src/pages/Foods/Foods.js
+++ src/pages/Foods/Foods.js
@@ -1,13 +1,113 @@
-import React from 'react';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-export default function Foods() {
-  return (
+import PropTypes from 'prop-types';
+import Card from '../../components/Card';
+import Header from '../../components/Header';
+import { FoodCtx } from '../../context/contextFood';
+import Footer from '../../components/Footer';
+import CategoryBtn from '../../components/Buttons/CategoryBtn';
+
+import './Foods.css';
+
+function Foods({ location: { state = false } }) {
+  const STOP_INDEX = 11;
+  const { foodApi: { meals }, setFilterFood } = useContext(FoodCtx);
+  const [category, setCategory] = useState('');
+  const [ingredient, setIngredient] = useState('');
+  const history = useHistory();
+  const onClickAll = ({ target }) => {
+    if (state && state.fromExplorerFoodsIngredients) {
+      state.fromExplorerFoodsIngredients = false;
+    }
+    setCategory(target.value);
+    setFilterFood({ key: 'name', value: category });
+  };
+  const onClickCategory = ({ target }) => {
+    if (category !== target.value) {
+      setCategory(target.value);
+      if (state && state.fromExplorerFoodsIngredients) {
+        state.fromExplorerFoodsIngredients = false;
+      }
+    } else { setCategory(''); }
+  };
+
+  useEffect(() => {
+    if (state !== undefined && state.fromExplorerFoodsIngredients) {
+      setIngredient(state.ingredient);
+      setFilterFood({ key: 'ing', value: ingredient });
+    } else {
+      setFilterFood({ key: 'category', value: category });
+    }
+  }, [category, setFilterFood, state, ingredient]);
+
+  useEffect(() => {
+    const categorySetting = (categoryState) => {
+      if (categoryState === '') {
+        setFilterFood({ key: 'name', value: categoryState });
+      }
+    }; categorySetting(category);
+  }, [category, setFilterFood]);
+
+  const render = () => (
     <div>
-      <Header title="Comidas" icon="true" currentPage="Foods" />
-      Foods
+      <Header name="Comidas" icon="true" currentPage="Foods" />
+      <CategoryBtn
+        label="Foods"
+        onClickAll={ onClickAll }
+        onClickCategory={ onClickCategory }
+      />
+      <div className="cards">
+        {meals && meals
+          .filter((meal, index) => index <= STOP_INDEX)
+          .map((item, index) => (
+            <Card
+              key={ item.idMeal }
+              id={ item.idMeal }
+              name={ item.strMeal }
+              img={ item.strMealThumb }
+              index={ index }
+              onClick={ () => history.push(`/comidas/${item.idMeal}`) }
+            />
+          ))}
+
+        { (meals && meals.length === 1 && category === '')
+          ? history.push(`/comidas/${meals[0].idMeal}`)
+          : '' }
+        { meals === null
+        // eslint-disable-next-line no-alert
+          ? alert('Sinto muito, não encontramos nenhuma receita para esses filtros.')
+          : ''}
+      </div>
+      <div className="spacing" />
       <Footer />
     </div>
   );
+
+  return (render());
 }
+
+Foods.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string,
+    search: PropTypes.string,
+    hash: PropTypes.string,
+    state: PropTypes.shape({
+      fromExplorerFoodsIngredients: PropTypes.bool,
+      ingredient: PropTypes.string,
+    }),
+    key: PropTypes.string,
+  }),
+};
+
+Foods.defaultProps = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string,
+    search: PropTypes.string,
+    hash: PropTypes.string,
+    state: PropTypes.string,
+    key: PropTypes.string,
+  }),
+};
+
+export default Foods;
