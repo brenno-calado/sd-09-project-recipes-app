341372b0a61f966983b05dd17167dd05c3b4f78e
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-05-03 15:00:23 -0300

    Feat: requisito perfil

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,8 +1,55 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { LoginContext } from '../context';
 
 function Perfil() {
+  const { values } = useContext(LoginContext);
+  const { user } = values;
+  console.log(user);
+  const history = useHistory();
+
+  const handleSubmitDone = () => {
+    history.push('/receitas-feitas');
+  };
+
+  const handleSubmitFavorite = () => {
+    history.push('/receitas-favoritas');
+  };
+
+  const handleSubmitLogin = () => {
+    history.push('/');
+  };
+
   return (
-    <h1> Perfil </h1>
+    <div>
+      <spam
+        value={ values.user }
+        data-testid="profile-email"
+      />
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ handleSubmitDone }
+      >
+        Receitas Feitas
+      </button>
+      <br />
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ handleSubmitFavorite }
+      >
+        Receitas Favoritas
+      </button>
+      <br />
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ handleSubmitLogin }
+      >
+        Sair
+      </button>
+    </div>
   );
 }
 
