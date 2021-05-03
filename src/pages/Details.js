71c9c7bc7affb80265996ce574aaf37bf93965f0
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-05-03 19:20:31 -0300

    Details Page Half-Done

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import YouTube from 'react-youtube';
+import getVideoId from 'get-video-id';
 
 import { fetchDrinkRecipeDetails, fetchMealRecipeDetails } from '../actions';
 
@@ -14,15 +16,47 @@ class MealDetails extends React.Component {
   }
 
   render() {
-    const { details } = this.props;
+    const { details, type } = this.props;
+    const detailsAlt = details || {};
     return (
       (type === 'meals') ? (
-        <div>
-          <img alt="Recipe" src={ details.strMealThumb } data-testid="recipe-photo" />
-        </div>
+        (
+          <div>
+            <img
+              alt="Recipe"
+              src={ detailsAlt.strMealThumb }
+              data-testid="recipe-photo"
+            />
+            <h1 data-testid="recipe-title">{detailsAlt.strMeal}</h1>
+            <p data-testid="recipe-category">{`Categoria: ${detailsAlt.strCategory}`}</p>
+            <h2>Ingredients:</h2>
+            <ul>
+              {
+                Array.from({ length: 20 }, (_, index) => `strIngredient${index + 1}`)
+                  .map((strIngredient, index) => (
+                    (detailsAlt[strIngredient]
+                      && (
+                        <li
+                          key={ index }
+                          data-testid={ `${index + 1}-ingredient-name-and-measure` }
+                        >
+                          {detailsAlt[strIngredient]}
+                        </li>
+                      )
+                    ) || null))
+              }
+            </ul>
+            <h2>Instructions:</h2>
+            <p data-testid="instructions">{ detailsAlt.strInstructions }</p>
+            {
+              (detailsAlt.strYoutube
+                && <YouTube videoId={ getVideoId(detailsAlt.strYoutube) } />) || null
+            }
+          </div>
+        ) || null
       ) : (
         <p>a</p>
-      )
+      ) || null
     );
   }
 }
