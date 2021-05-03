c7621e546fac26ceea2a740eea28649b01738686
LucioOSilva <lucioosilva00@gmail.com>
2021-05-03 10:59:10 -0300

    feat: added requirement 43

--- src/components/DetailsBtnShareRecipe.jsx
+++ src/components/DetailsBtnShareRecipe.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
+import './DetailsBtnShareRecipe.css';
 import { Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
 function DetailsBtnShareRecipe() {
+  function removeTextFromComponent(component) {
+    const maxTimeToRemoveElement = 1500;
+    setTimeout(() => {
+      component.lastChild.innerText = '';
+    }, maxTimeToRemoveElement);
+  }
+
+  async function handleClickCopy() {
+    const tooltip = document.getElementById('myTooltip');
+    await navigator.clipboard.writeText(window.location.href);
+    tooltip.innerText = 'Link copiado!';
+    removeTextFromComponent(tooltip.parentElement);
+  }
+
   return (
-    <Button
-      data-testid="share-btn"
-      type="button"
-      color="primary"
-    >
-      Share
-    </Button>
+    <div className="boxbtn">
+      <Button
+        data-testid="share-btn"
+        type="button"
+        color="primary"
+        onClick={ handleClickCopy }
+      >
+        Share
+      </Button>
+      <span className="tooltiptext" id="myTooltip" />
+    </div>
   );
 }
+
+DetailsBtnShareRecipe.propTypes = { detailsContext: PropTypes.object }.isRequired;
 
 export default DetailsBtnShareRecipe;
