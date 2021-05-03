9f636494478981204e0890a83ca62d89593626fe
Thiago Granville <thiagogranville@gmail.com>
2021-05-03 18:48:34 -0300

    [FEAT][Requisito 40][Implementa solução de continuar receita]

--- src/components/StartRecipeButton.js
+++ src/components/StartRecipeButton.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { string } from 'prop-types';
 import { Link } from 'react-router-dom';
 
+import {
+  addRecipeInProgress,
+  checkRecipesInProgress } from '../services/inProgressRecipes';
+import RecipesContext from '../context/RecipesContext';
 import './css/StartRecipeButton.css';
 
 const StartRecipeButton = (props) => {
   const { pathname } = props;
+  const { recipesType, changeRecipesType } = useContext(RecipesContext);
+  const [inProgress, setProgress] = useState(false);
+  const idRegex = new RegExp('[0-9]+');
+  const id = pathname.match(idRegex)[0];
+
+  useEffect(() => {
+    changeRecipesType();
+    checkRecipesInProgress(recipesType, id, setProgress);
+  }, [id, recipesType]);
 
   return (
     <div>
@@ -14,8 +27,9 @@ const StartRecipeButton = (props) => {
           className="btnStart btn btn-primary btn-block"
           type="button"
           data-testid="start-recipe-btn"
+          onClick={ () => { if (!inProgress) addRecipeInProgress(recipesType, id); } }
         >
-          Iniciar Receita
+          { inProgress ? 'Continuar Receita' : 'Iniciar Receita' }
         </button>
       </Link>
     </div>
