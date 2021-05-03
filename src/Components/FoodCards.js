ca4f5add68e487302bcade7908a335b5faab4ce6
Sabrina Alves Delpache <sasa2605.alves@gmail.com>
2021-05-03 17:46:56 -0300

    Requisito 29 e 31 - feitos

--- src/Components/FoodCards.js
+++ src/Components/FoodCards.js
@@ -14,6 +14,7 @@ class FoodCards extends React.Component {
     this.updateSearchedMeal = this.updateSearchedMeal.bind(this);
     this.state = {
       filteredByCategories: [],
+      toggleCategory: '',
     };
   }
 
@@ -31,10 +32,14 @@ class FoodCards extends React.Component {
   }
 
   async updateSearchedMeal(category) {
-    const categoryResponse = await fetchMealByCategory(category);
-    this.setState({
-      filteredByCategories: categoryResponse.meals,
-    });
+    const { toggleCategory } = this.state;
+    this.setState({ toggleCategory: category });
+    if (category === toggleCategory) this.setState({ filteredByCategories: null });
+    else if (category === 'All') this.setState({ filteredByCategories: null });
+    else {
+      const categoryResponse = await fetchMealByCategory(category);
+      this.setState({ filteredByCategories: categoryResponse.meals });
+    }
   }
 
   createCards() {
