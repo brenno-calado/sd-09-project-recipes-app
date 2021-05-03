d0682e66c436bb2648dc57d95131ac63b303c3ec
Diego Vinicius Matheus Martins <analistamm@gmail.com>
2021-05-03 09:28:32 -0300

    Update src/Pages/Explorar.js
    Co-authored-by: ChrisMoura2000 <74259597+ChrisMoura2000@users.noreply.github.com>

--- src/Pages/Explorar.js
+++ src/Pages/Explorar.js
@@ -5,7 +5,7 @@ import Footer from '../components/Footer';
 
 function Explorar() {
   return (
-    <div className="">
+    <div>
       <Link to="/explorar/comidas">
         <button
           className=""
