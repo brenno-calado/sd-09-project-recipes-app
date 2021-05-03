f7659a7d7251aff3f61727b4d65e16a806450740
Carine Becker <caarine.m@gmail.com>
2021-05-03 16:05:05 -0300

    feat: in progress page part of

--- src/pages/InProgress.jsx
+++ src/pages/InProgress.jsx
@@ -1,37 +1,9 @@
 import React from 'react';
-import { meals } from '../mock/mockMeals.json'
+import DetailsInProgress from '../components/DetailsInProgress';
 
 function InProgress() {
-  const { strMeal, strMealThumb } = meals[0];
   return (
-    <main>
-      <div className="img-container">
-        <img
-          src={ strMealThumb }
-          className="details-recipe-photo"
-          data-testid="recipe-photo"
-          alt={ strMeal }
-        />
-      </div>
-      <h2
-        data-testid="recipe-title"
-      >
-        { strMeal }
-      </h2>
-      <button 
-        href="#share"
-        data-testid="share-btn"
-      >
-        <img src="" alt="Share button" />
-      </button>
-      <button
-        href="#favorites"
-        data-testid="favorite-btn"
-      >
-        <img src="" alt="Favorite button"/>
-      </button>
-
-    </main>
+    <DetailsInProgress />
   );
 }
 
