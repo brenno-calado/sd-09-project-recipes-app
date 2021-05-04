f976c6f2e0a58eb4759ca8980048f599e1f20069
Felipe Cardoso <felipedclc@gmail.com>
2021-05-04 17:02:03 -0300

    req33-incomplete

--- src/components/RecipeDetailsCard.js
+++ src/components/RecipeDetailsCard.js
@@ -1,14 +1,26 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { AiOutlineShareAlt } from 'react-icons/ai';
 import { GrFavorite } from 'react-icons/gr';
 import { useParams } from 'react-router';
 import ReactPlayer from 'react-player';
-import { RecipiesContext } from '../context/RecipiesContext';
 import { getRecipesById } from '../services/api';
 
+const getIngredientsAndMeasure = (recipe) => {
+  let ingredientsList = [];
+  for (let i = 1; i <= Number('20'); i += 1) {
+    const newItem = {
+      ingredient: recipe[`strIngredient${i}`],
+      measure: recipe[`strMeasure${i}`],
+    };
+    if (newItem && newItem.ingredient.length === 0) break;
+    ingredientsList = [...ingredientsList, newItem];
+  }
+  return ingredientsList;
+};
+
 function RecipesDetailsCard({ isMeal }) {
-  const { recipeDetails, setRecipeDetails } = useContext(RecipiesContext);
+  const [recipeDetails, setRecipeDetails] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
@@ -19,104 +31,44 @@ function RecipesDetailsCard({ isMeal }) {
     getRecipe();
   }, [id, isMeal, setRecipeDetails]);
 
-  console.log(recipeDetails);
-  /* const getRecipe = (recipe, idType) => (
-    Object.values(recipe).find((mealOrDrink) => mealOrDrink[idType] === id)
-  ); */
+  const renderIngredientsAndMeasure = () => getIngredientsAndMeasure(recipeDetails)
+    .map(({ ingredient, measure }, index) => (
+      <li
+        key={ ingredient }
+        data-testid={ `${index}-ingredient-name-and-measure` }
+      >
+        { `${ingredient} - ${measure}` }
+      </li>
+    ));
 
-  /* const getIngredientsAndMeasure = (recipe) => {
-    let ingredientsList = [];
-    for (let i = 1; i <= Number('20'); i += 1) {
-      const newItem = {
-        ingredient: recipe[`strIngredient${i}`],
-        measure: recipe[`strMeasure${i}`],
-      };
-      if (newItem && newItem.ingredient.length === 0) break;
-      ingredientsList = [...ingredientsList, newItem];
-    }
-    return ingredientsList;
-  };
-
-  const renderIngredientsAndMeasure = () => {
-    if (isMeal && searchMealsList) {
-      const mealRecipe = getRecipe(searchMealsList, 'idMeal');
-      return mealRecipe && getIngredientsAndMeasure(mealRecipe)
-        .map(({ ingredient, measure }, index) => (
-          <li
-            key={ ingredient }
-            data-testid={ `${index}-ingredient-name-and-measure` }
-          >
-            { `${ingredient} - ${measure}` }
-          </li>
-        ));
-    }
-    if (isMeal === false && searchDrinksList) {
-      const drinkRecipe = getRecipe(searchDrinksList, 'idDrink');
-      return drinkRecipe && getIngredientsAndMeasure(drinkRecipe)
-        .map(({ ingredient, measure }) => (
-          <li key={ ingredient }>{ `${ingredient} - ${measure}` }</li>
-        ));
-    }
-  };
-
-  const renderImg = () => (
-    <img
-      src={ isMeal
-        ? getRecipe(searchMealsList, 'idMeal').strMealThumb
-        : getRecipe(searchDrinksList, 'idDrink').strDrinkThumb }
-      alt="Foto da receita"
-      data-testid="recipe-photo"
-      width="300"
-    />
-  );
-
-  const renderTitle = () => (
-    <h1 data-testid="recipe-title">
-      { isMeal
-        ? getRecipe(searchMealsList, 'idMeal').strMeal
-        : getRecipe(searchDrinksList, 'idDrink').strDrink }
-    </h1>
-  );
-
-  const renderCategory = () => (
-    <h2 data-testid="recipe-category">
-      {isMeal
-        ? getRecipe(searchMealsList, 'idMeal').strCategory
-        : getRecipe(searchDrinksList, 'idDrink').strCategory }
-    </h2>
-  );
-
-  const renderInstructions = () => (
-    <p data-testid="instructions">
-      {isMeal
-        ? getRecipe(searchMealsList, 'idMeal').strInstructions
-        : getRecipe(searchDrinksList, 'idDrink').strInstructions}
-    </p>
-  );
-
-  const renderVideo = () => (
-    <div data-testid="video">
-      {isMeal && <ReactPlayer url={ getRecipe(searchMealsList, 'idMeal').strYoutube } />}
-    </div>
-  ); */
+  if (!recipeDetails) return <p>Loading...</p>;
 
   return (
     <section>
-      {/* {renderImg()}
-      {renderTitle()}
+      <img
+        src={ isMeal ? recipeDetails.strMealThumb : recipeDetails.strDrinkThumb }
+        alt="Foto da receita"
+        data-testid="recipe-photo"
+        width="300"
+      />
+      <h1 data-testid="recipe-title">
+        { isMeal ? recipeDetails.strMeal : recipeDetails.strDrink }
+      </h1>
       <button type="button" data-testid="share-btn">
         <AiOutlineShareAlt />
       </button>
       <button type="button" data-testid="favorite-btn">
         <GrFavorite />
       </button>
-      {renderCategory()}
+      <h2 data-testid="recipe-category">{recipeDetails.strCategory }</h2>
       Ingredientes
       <ul>
         {renderIngredientsAndMeasure()}
       </ul>
-      {renderInstructions()}
-      {renderVideo()} */}
+      <p data-testid="instructions">{recipeDetails.strInstructions}</p>
+      <div data-testid="video">
+        {isMeal && <ReactPlayer url={ recipeDetails.strYoutube } />}
+      </div>
       {/* <div data-testid={ `${index}-recomendation-card` }>Recomendações</div> */}
       <button type="button" data-testid="start-recipe-btn">
         Iniciar receita
