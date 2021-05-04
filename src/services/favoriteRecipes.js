b201b2820853e70d6c6fcb84969b459650661919
lcgoandete <lcgoandete@hotmail.com>
2021-05-04 18:52:11 -0300

    [FEAT][Requisito 44-45-46][implementacao do botao de favoritos]

--- src/services/favoriteRecipes.js
+++ src/services/favoriteRecipes.js
@@ -1,5 +1,33 @@
-const saveFavoriteRecipesInLocalStorage = (favoriteRecipes) => {
-  localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes));
+const loadFavoriteRecipesFromLocalStorage = () => (
+  JSON.parse(localStorage.getItem('favoriteRecipes'))
+);
+
+const removeFavoriteRecipesFromLocalStorageById = (id) => {
+  const favoriteRecipes = loadFavoriteRecipesFromLocalStorage();
+  const newfavoriteRecipes = favoriteRecipes
+    .filter((favoriteRecipe) => favoriteRecipe.id !== id.toString());
+  localStorage.setItem('favoriteRecipes', JSON.stringify(newfavoriteRecipes));
 };
 
-export default saveFavoriteRecipesInLocalStorage;
+const saveFavoriteRecipesInLocalStorage = (favoriteRecipe) => {
+  let favoriteRecipes = loadFavoriteRecipesFromLocalStorage();
+  favoriteRecipes = favoriteRecipes === null ? [] : favoriteRecipes;
+  localStorage.setItem('favoriteRecipes',
+    JSON.stringify(favoriteRecipes.concat(favoriteRecipe)));
+};
+
+const getFavoriteRecipesFromLocalStorageById = (id) => {
+  try {
+    return loadFavoriteRecipesFromLocalStorage()
+      .some((favoriteRecipe) => favoriteRecipe.id === id.toString());
+  } catch (error) {
+    return false;
+  }
+};
+
+export {
+  saveFavoriteRecipesInLocalStorage,
+  loadFavoriteRecipesFromLocalStorage,
+  getFavoriteRecipesFromLocalStorageById,
+  removeFavoriteRecipesFromLocalStorageById,
+};
