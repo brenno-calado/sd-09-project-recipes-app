700192eea752bd21ee47f05d29de94222aea0189
Mateus <mateussantiago.victor@gmail.com>
2021-05-04 20:21:53 -0300

    Paginas de receitas em progresso

--- src/Pages/DrinkProgress.js
+++ src/Pages/DrinkProgress.js
@@ -5,17 +5,16 @@ import FavoriteButton from '../Components/FavoriteButton';
 import Share from '../Components/Share';
 import { fetchDrinkDetailsAPI } from '../services/ApiRequest';
 
+const MAX_NUMBER_INGREDIENTS = 3;
 class DrinkProgress extends Component {
   constructor() {
     super();
-    this.sumblimeText = this.sumblimeText.bind(this);
-    // this.addLocalStorage = this.addLocalStorage.bind(this);
-    // this.checklocalStorage = this.checklocalStorage.bind(this);
-    // this.checkedItems = this.checkedItems.bind(this);
     this.state = {
       drink: {},
       igredients: [],
     };
+    this.sumblimeText = this.sumblimeText.bind(this);
+    this.checkedItems = this.checkedItems.bind(this);
   }
 
   componentDidMount() {
@@ -32,6 +31,19 @@ class DrinkProgress extends Component {
       });
   }
 
+  checkedItems() {
+    const validetion = 0;
+    const checkeds = document.querySelectorAll('input');
+    const button = document.querySelector('#finalizar');
+    checkeds.forEach((checked) => {
+      if (checked.checked === true) {
+        console.log('dd');
+      }
+    });
+    if (validetion === checkeds.length) button.disabled = true;
+    if (validetion !== checkeds.length) button.disabled = false;
+  }
+
   sumblimeText() {
     const label = document.querySelectorAll('label');
     const input = document.querySelectorAll('input');
@@ -41,100 +53,10 @@ class DrinkProgress extends Component {
     });
   }
 
-  // checkedItems() {
-  //   let validetion = 0;
-  //   const checkeds = document.querySelectorAll('input');
-  //   const button = document.querySelector('#finalizar');
-  //   checkeds.forEach((checked) => {
-  //     if (checked.checked === true) {
-  //       console.log('dd');
-  //     }
-  //   });
-  //   if (validetion === checkeds.length) button.disabled = true;
-  //   if (validetion <= checkeds.length) button.disabled = false;
-  // }
-
-  // addLocalStorage() {
-  //    const { drink } = this.state;
-  //    const div = document.querySelector('#items');
-  //    const obj = JSON.parse(localStorage.getItem('inProgressRecipes'));
-  //    if (obj !== null) {
-  //      localStorage.setItem('inProgressRecipes',
-  //        JSON.stringify({
-  //          cocktails:{
-  //            ...obj.cocktails,
-  //            [drink.idDrink]:[ div.innerHTML]
-  //          }
-  //        })
-  //      );
-  //    }
-  //    if (obj === null) {
-  //      localStorage.setItem('inProgressRecipes',
-  //        JSON.stringify({
-  //          cocktails:{
-  //            [drink.idDrink]:[ div.innerHTML]
-  //          }
-  //        })
-  //      );
-  //    }
-  // }
-
-  // checklocalStorage(id) {
-  //   const obj = JSON.parse(localStorage.getItem('inProgressRecipes'));
-  //   const div = document.querySelectorAll('#items')
-  //   if (obj === null) {
-  //     console.log([div], 'aqui');
-  //     localStorage.setItem('inProgressRecipes',
-  //       JSON.stringify({
-  //         meals:{},
-  //         cocktails:{
-  //           [id]: [div[0].innerHTML]
-  //         }
-  //       })
-  //     );
-  //   }
-
-  //   if(obj !== null) {
-  //     const idDrink = Object.keys(obj.cocktails).some((key) => key === id);
-  //     if (idDrink === true) {
-  //       this.setState({ ok: true });
-  //       console.log(obj.cocktails[id][0]);
-  //     }
-  //   }
-  // }
-
-  // ingredients(drink, igredients) {
-  //   return (
-  //     <div id="items">
-  //       {
-  //         igredients.map((value, index) => (
-  //           if (drink[value] !== null) {
-  //             return (
-  //               <label
-  //                 onClick={ this.checkedItems }
-  //                 onClick={ this.sumblimeText }
-  //                 key={ index }
-  //                 htmlFor={ index }
-  //                 data-testid={ `${index}-ingredient-step` }
-  //               >
-  //                 {drink[value]}
-  //                 <input
-  //                   id={ index }
-  //                   type="checkbox"
-  //                 />
-  //               </label>
-  //             );
-  //           }
-  //         ))
-  //       }
-  //     </div>
-  //   );
-  // }
-
   render() {
     const { drink, igredients } = this.state;
-    const { match } = this.props;
-    const { params: { id } } = match;
+    // const { match } = this.props;
+    // const { params: { id } } = match;
     const { strCategory, strDrinkThumb, strDrink, strInstructions } = drink;
     return (
       <div>
@@ -144,14 +66,34 @@ class DrinkProgress extends Component {
         <img data-testid="recipe-photo" src={ strDrinkThumb } alt={ strDrink } />
         <h2 data-testid="recipe-title">{strDrink}</h2>
         <h3 data-testid="recipe-category">{strCategory}</h3>
-        {() => this.igredients(drink, igredients)}
+        <div>
+          {igredients.map((value, index) => {
+            if (drink[value] !== null && index < MAX_NUMBER_INGREDIENTS) {
+              return (
+                <label
+                  key={ index }
+                  htmlFor={ index }
+                  data-testid={ `${index}-ingredient-step` }
+                >
+                  {drink[value]}
+                  <input
+                    id={ index }
+                    type="checkbox"
+                    onClick={ this.sumblimeText }
+                  />
+                </label>
+              );
+            }
+            return false;
+          })}
+        </div>
         <p data-testid="instructions">{strInstructions}</p>
         <Link
-          to={ `/bebidas/${id}/receitas-feitas` }
+          to="/receitas-feitas"
         >
           <button
-            id="finalizar"
             type="button"
+            disabled={ !this.checkedItems }
             data-testid="finish-recipe-btn"
           >
             Finalizar Receita
