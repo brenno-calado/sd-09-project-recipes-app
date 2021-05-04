442235462eed7162009e218aab60804b909146d6
Enio Nicael <enionicael@gmail.com>
2021-05-04 18:10:58 -0300

    Solucao de bug na leitura do localStorage.

--- src/services/getLocalStorageDoneRecipes.js
+++ src/services/getLocalStorageDoneRecipes.js
@@ -2,6 +2,7 @@ const getDoneRecipes = () => {
   if (localStorage.length !== 0) {
     return JSON.parse(localStorage.getItem('doneRecipes'));
   }
+  return [];
 };
 
 export default getDoneRecipes;
