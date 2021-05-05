7b5b5678b65cf1ee8483b798c71c8e4720174393
hannibal1207 <mathausannibal@gmail.com>
2021-05-05 18:44:27 -0300

    adiciona req 78

--- src/pages/ExploreFoodArea.jsx
+++ src/pages/ExploreFoodArea.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import ExploreFoodAreaa from '../components/ExploreFoodAreaa/index';
 
 function ExploreFoodArea({ match: { path } }) {
   return (
     <>
       <Header title="Explorar Origem" path={ path } />
+      <ExploreFoodAreaa />
       <Footer />
     </>
   );
