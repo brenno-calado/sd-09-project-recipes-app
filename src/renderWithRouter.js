5282bf6771dfc236e7581b56a1ea381df4fdc814
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-05-05 14:42:51 -0300

    eslint issues in testData

--- src/renderWithRouter.js
+++ src/renderWithRouter.js
@@ -3,7 +3,6 @@ import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import RecipesProvider from './context/RecipesContext';
 import store from './store';
 
 const renderWithRouter = (component) => {
@@ -11,9 +10,7 @@ const renderWithRouter = (component) => {
   return ({
     ...render(
       <Provider store={ store }>
-        {/* <RecipesProvider> */}
         <Router history={ history }>{component}</Router>
-        {/* </RecipesProvider> */}
       </Provider>,
     ),
     history,
