1790a15d5b3831ddeb6c26795cf8de03e49e2c2f
nyxnil <nsmnilsonn@gmail.com>
2021-05-05 16:58:37 -0300

    pagina profile req 82-87

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,55 @@
 import React from 'react';
-import Header from '../../components/Header';
+import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer';
+import Header from '../../components/Header';
 
-export default function Profile() {
+function Perfil() {
+  const logout = () => {
+    localStorage.clear();
+  };
+
+  const userEmail = JSON.parse(localStorage.getItem('user')) || '';
+
   return (
-    <div>
-      <Header title="Perfil" currentPage="Profile" icon="false" />
-      Perfil
+    <>
+      <Header explore="false">Perfil</Header>
+      <section>
+        <span
+          data-testid="profile-email"
+        >
+          {userEmail.email}
+        </span>
+        <div>
+          <button
+            data-testid="profile-done-btn"
+            type="button"
+          >
+            <Link to="/receitas-feitas">
+              Receitas Feitas
+          </Link>
+          </button>
+          <button
+            data-testid="profile-favorite-btn"
+            type="button"
+          >
+            <Link to="/receitas-favoritas">
+              Receitas Favoritas
+          </Link>
+          </button>
+          <button
+            data-testid="profile-logout-btn"
+            type="button"
+            onClick={logout}
+          >
+            <Link to="/">
+              Sair
+          </Link>
+          </button>
+        </div>
+      </section>
       <Footer />
-    </div>
+    </>
   );
 }
+
+export default Perfil;
