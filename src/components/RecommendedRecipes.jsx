cc09eb923ff5317f1a6b3a4adfcdaa24844c191f
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-05-05 13:36:31 -0300

    Removi trechos de codigos comentados

--- src/components/RecommendedRecipes.jsx
+++ src/components/RecommendedRecipes.jsx
@@ -12,10 +12,7 @@ function RecommendedRecipes({
   cocktailsThunkDispatcher,
   recipeType,
   recommended,
-  // isRecommendedFetched,
 }) {
-  // const [data, setData] = useState([]);
-
   const verifyRecipeType = useCallback(() => {
     if (recipeType === 'comidas') {
       cocktailsThunkDispatcher();
@@ -25,52 +22,12 @@ function RecommendedRecipes({
     }
   }, [recipeType, cocktailsThunkDispatcher, mealsThunkDispatcher]);
 
-  // const verifyFetch = useCallback(() => {
-  //   if (isRecommendedFetched) {
-  //     const magicNumber = 6;
-  //     setData(recommended.slice(0, magicNumber));
-  //   }
-  // }, [isRecommendedFetched, setData, recommended]);
-
   useEffect(() => {
     verifyRecipeType();
   }, [verifyRecipeType]);
 
-  // useEffect(() => {
-  //   verifyFetch();
-  // }, [recommended, verifyFetch]);
-
-  // const recommendedMeals = data.map((recipe, index) => (
-  //   <div
-  //     key={ recipe.idMeal }
-  //     data-testid={ `${index}-recomendation-card` }
-  //     className="recommendation-card"
-  //   >
-  //     <img
-  //       src={ recipe.strMealThumb }
-  //       alt="foto da receita"
-  //     />
-  //     <p>{recipe.strMeal}</p>
-  //   </div>
-  // ));
-
-  // const recommendedDrinks = data.map((recipe, index) => (
-  //   <div
-  //     key={ recipe.idDrink }
-  //     data-testid={ `${index}-recomendation-card` }
-  //     className="recommendation-card"
-  //   >
-  //     <img
-  //       src={ recipe.strDrinkThumb }
-  //       alt="foto da receita"
-  //     />
-  //     <p>{recipe.strDrink}</p>
-  //   </div>
-  // ));
-
   return (
     <section className="recommendation-container">
-      {/* { recipeType === 'comidas' ? recommendedDrinks : recommendedMeals } */}
       <HorizontalScrollMenu recommended={ recommended } />
     </section>
   );
@@ -79,7 +36,6 @@ function RecommendedRecipes({
 const mapStateToProps = (state) => ({
   recipeType: state.recipesReducer.recipeType,
   recommended: state.recipeDetailsReducer.recommended,
-  // isRecommendedFetched: state.recipeDetailsReducer.isRecommendedFetched,
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -92,7 +48,6 @@ RecommendedRecipes.propTypes = {
   cocktailsThunkDispatcher: PropTypes.func.isRequired,
   recipeType: PropTypes.string.isRequired,
   recommended: PropTypes.arrayOf(PropTypes.object).isRequired,
-  // isRecommendedFetched: PropTypes.bool.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(RecommendedRecipes);
