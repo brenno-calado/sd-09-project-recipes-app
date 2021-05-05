db009af4fbb79ddce033d44608b00ca655c3a5d3
Thiago Granville <thiagogranville@gmail.com>
2021-05-05 16:49:46 -0300

    [FEAT][Requisito 77][Redireciona usuário ao clicar em um ingrediente]

--- src/components/Ingredients/Index.js
+++ src/components/Ingredients/Index.js
@@ -1,41 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CardDeck from 'react-bootstrap/CardDeck';
 import Card from 'react-bootstrap/Card';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const Ingredients = ({ ingredientsList, explorerPath }) => (
-  <div>
-    <CardDeck>
-      { ingredientsList.map(
-        (ingredient, index) => (
-          <Card
-            data-testid={ `${index}-ingredient-card` }
-            key={ index }
-          >
-            <Link key={ index } to={ `/${explorerPath}` }>
+import { getDrinksByIngredient } from '../../services/DrinksAPI';
+import { getFoodByIngredient } from '../../services/FoodAPI';
+import RecipesContext from '../../context/RecipesContext';
+
+const Ingredients = ({ ingredientsList, explorerPath }) => {
+  const { requestSearch } = useContext(RecipesContext);
+  return (
+    <div>
+      <CardDeck>
+        { ingredientsList.map((ingredient, index) => (
+          <Card data-testid={ `${index}-ingredient-card` } key={ index }>
+            <Link
+              key={ index }
+              to={ `/${explorerPath}` }
+              onClick={ () => requestSearch(
+                getFoodByIngredient,
+                getDrinksByIngredient,
+                ingredient.name,
+              ) }
+            >
               <Card.Img
                 variant="top"
                 src={ ingredient.thumbnailURL }
                 data-testid={ `${index}-card-img` }
               />
               <Card.Body>
-                <Card.Title
-                  data-testid={ `${index}-card-name` }
-                >
+                <Card.Title data-testid={ `${index}-card-name` }>
                   { ingredient.name }
                 </Card.Title>
               </Card.Body>
             </Link>
-          </Card>),
-      )}
-    </CardDeck>
-  </div>
-);
+          </Card>
+        ))}
+      </CardDeck>
+    </div>
+  );
+};
 Ingredients.propTypes = {
-  ingredientsList: PropTypes.oneOfType(
-    [PropTypes.array, PropTypes.object],
-  ).isRequired,
+  ingredientsList: PropTypes.oneOfType([PropTypes.array, PropTypes.object])
+    .isRequired,
   explorerPath: PropTypes.string.isRequired,
 };
 export default Ingredients;
