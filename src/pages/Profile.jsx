658af77ab6ea8fcca264c8f89f53cbe791b11e63
Matheus Martino <matheus.martino97@gmail.com>
2021-05-05 17:11:19 -0300

    Requisito 82.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import './Styles/Profile.css';
 
 function Profile() {
+  const { email } = JSON.parse(localStorage.getItem('user'));
+
   return (
-    <div>
+    <div className="Profile">
       <Header title="Perfil" />
+      <main>
+        <h1 className="email" data-testid="profile-email">
+          {email}
+        </h1>
+        <div className="buttons">
+          <button
+            data-testid="profile-done-btn"
+            className="button"
+            type="button"
+          >
+            Receitas Feitas
+          </button>
+          <button
+            data-testid="profile-favorite-btn"
+            className="button"
+            type="button"
+          >
+            Receitas Favoritas
+          </button>
+          <button
+            data-testid="profile-logout-btn"
+            className="button"
+            type="button"
+          >
+            Sair
+          </button>
+        </div>
+      </main>
       <Footer />
     </div>
   );
