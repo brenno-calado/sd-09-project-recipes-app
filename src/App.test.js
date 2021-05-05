24161f0928d5e9c1c5f89b93c40dc21c1cfc50aa
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-05-05 14:41:56 -0300

    eslint issues in testData

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import App from './App';
+import React from 'react';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import { Router, MemoryRouter } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import App from './App';
+import renderWithRouter from './renderWithRouter';
+import RecipesProvider from './context/RecipesContext';
+import { meals, drinks } from './testData';
 
-// test('Farewell, front-end', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/TRYBE/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+const mockFetchMeals = () => {
+  jest.spyOn(global, 'fetch')
+    .mockImplementation(() => Promise.resolve({
+      status: 200,
+      ok: true,
+      json: () => Promise.resolve(meals)
+    }));
+}
+const mockFetchDrinks = () => {
+  jest.spyOn(global, 'fetch')
+    .mockImplementation(() => Promise.resolve({
+      status: 200,
+      ok: true,
+      json: () => Promise.resolve(drinks)
+    }));
+}
+
+describe('1 - Testes da Pagina de login', () => {
+  it('A rota "/" deve renderizar a pagina de login', () => {
+    const { history } = renderWithRouter(<App />);
+    const url = history.location.pathname;
+    expect(url).toEqual('/');
+  })
+})
--- src/testData.js
+++ src/testData.js
@@ -7,7 +7,7 @@ const meals = {
       strCategory: 'Side',
       strArea: 'Turkish',
       strInstructions:
-        'Pick through your lentils for any foreign debris, rinse them 2 or 3 times, drain, and set aside.  Fair warning, this will probably turn your lentils into a solid block that you’ll have to break up later\r\nIn a large pot over medium-high heat, sauté the olive oil and the onion with a pinch of salt for about 3 minutes, then add the carrots and cook for another 3 minutes.\r\nAdd the tomato paste and stir it around for around 1 minute. Now add the cumin, paprika, mint, thyme, black pepper, and red pepper as quickly as you can and stir for 10 seconds to bloom the spices. Congratulate yourself on how amazing your house now smells.\r\nImmediately add the lentils, water, broth, and salt. Bring the soup to a (gentle) boil.\r\nAfter it has come to a boil, reduce heat to medium-low, cover the pot halfway, and cook for 15-20 minutes or until the lentils have fallen apart and the carrots are completely cooked.\r\nAfter the soup has cooked and the lentils are tender, blend the soup either in a blender or simply use a hand blender to reach the consistency you desire. Taste for seasoning and add more salt if necessary.\r\nServe with crushed-up crackers, torn up bread, or something else to add some extra thickness.  You could also use a traditional thickener (like cornstarch or flour), but I prefer to add crackers for some texture and saltiness.  Makes great leftovers, stays good in the fridge for about a week.',
+        'Pick through your lentils for any foreign debris...',
       strMealThumb: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
       strTags: 'Soup',
       strYoutube: 'https://www.youtube.com/watch?v=VVnZd8A84z4',
@@ -63,7 +63,7 @@ const meals = {
       strCategory: 'Side',
       strArea: 'Turkish',
       strInstructions:
-        'If you order kumpir in Turkey, the standard filling is first, lots of butter mashed into the potato, followed by cheese. There’s then a row of other toppings that you can just point at to your heart’s content – sweetcorn, olives, salami, coleslaw, Russian salad, allsorts – and you walk away with an over-stuffed potato because you got ever-excited by the choices on offer.\r\n\r\nGrate (roughly – you can use as much as you like) 150g of cheese.\r\nFinely chop one onion and one sweet red pepper.\r\nPut these ingredients into a large bowl with a good sprinkling of salt and pepper, chilli flakes (optional).',
+        'If you order kumpir in Turkey...',
       strMealThumb: 'https://www.themealdb.com/images/media/meals/mlchx21564916997.jpg',
       strTags: 'SideDish',
       strYoutube: 'https://www.youtube.com/watch?v=IEDEtZ4UVtI',
@@ -128,15 +128,7 @@ const drinks = {
       strAlcoholic: 'Optional alcohol',
       strGlass: 'Collins Glass',
       strInstructions:
-        'Pour the Galliano liqueur over ice. Fill the remainder of the glass with ginger ale and thats all there is to it. You now have a your very own GG.',
-      strInstructionsES: null,
-      strInstructionsDE:
-        'Den Galliano-Likör über Eis gießen. Füllen Sie den Rest des Glases mit Ginger Ale und das ist alles, was dazu gehört. Du hast jetzt ein eigenes GG.',
-      strInstructionsFR: null,
-      strInstructionsIT:
-        'Versare il liquore Galliano su ghiaccio.\r\nRiempi il resto del bicchiere con ginger ale e questo è tutto.\r\nOra hai il tuo GG personale.',
-      'strInstructionsZH-HANS': null,
-      'strInstructionsZH-HANT': null,
+        'Pour the Galliano liqueur over ice...',
       strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
       strIngredient1: 'Galliano',
       strIngredient2: 'Ginger ale',
@@ -184,15 +176,7 @@ const drinks = {
       strAlcoholic: 'Alcoholic',
       strGlass: 'Cocktail glass',
       strInstructions:
-        'Pour all ingredients into a cocktail shaker, mix and serve over ice into a chilled glass.',
-      strInstructionsES: null,
-      strInstructionsDE:
-        'Alle Zutaten in einen Cocktailshaker geben, mischen und über Eis in ein gekühltes Glas servieren.',
-      strInstructionsFR: null,
-      strInstructionsIT:
-        'Versare tutti gli ingredienti in uno shaker, mescolare e servire con ghiaccio in un bicchiere freddo.',
-      'strInstructionsZH-HANS': null,
-      'strInstructionsZH-HANT': null,
+        'Pour all ingredients into a cocktail shaker...',
       strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg',
       strIngredient1: 'Gin',
       strIngredient2: 'Grand Marnier',
