7691075eca3e6d185bf66de49c7b43e3539f435a
BBrolesi <brunohsbrolesi@gmail.com>
2021-05-05 17:57:58 -0300

    fix: searchBar initial state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
@@ -10,21 +11,22 @@ const toggleSearchBar = (searchBarView, setSearchBarView) => {
 
 const Header = (props) => {
   const { title } = props;
-  const [searchBarView, setSearchBarView] = useState(true);
+  const [searchBarView, setSearchBarView] = useState(false);
   return (
     <header>
-      <img
-        src={ profileIcon }
-        alt="Icone de perfil"
-        data-testid="profile-top-btn"
-      />
+      <Link to="/perfil">
+        <img
+          src={ profileIcon }
+          alt="Icone de perfil"
+          data-testid="profile-top-btn"
+        />
+      </Link>
       <h1 data-testid="page-title">{title}</h1>
       <button
         type="button"
         onClick={ () => toggleSearchBar(searchBarView, setSearchBarView) }
-        data-testid="search-top-btn"
       >
-        <img src={ searchIcon } alt="Icone de perfil" />
+        <img src={ searchIcon } alt="Icone de perfil" data-testid="search-top-btn" />
       </button>
       {searchBarView ? <SearchBar /> : <span /> }
     </header>
