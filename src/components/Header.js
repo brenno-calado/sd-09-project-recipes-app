aa2df431d23100dd8c3ee4176faccc77dc73ae65
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-05-05 17:05:22 -0300

    score

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import searchImg from '../images/searchIcon.svg';
 import profileImg from '../images/profileIcon.svg';
+import searchImg from '../images/searchIcon.svg';
 import { RecipiesContext } from '../context/RecipiesContext';
 
 function CreateHeader({ title, showExplorerButton }) {
