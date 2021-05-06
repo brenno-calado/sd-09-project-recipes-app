d1bdc7519838257c47cd5585d06e7e199e4c7110
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-05-06 10:02:43 -0300

    Adiciona a page Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const user = JSON.parse(localStorage.getItem('user'));
+
   return (
     <div>
       <h1 data-testid="page-title">Perfil</h1>
       <Header />
+      <h3 data-testid="profile-email">{ user.email }</h3>
+      <div>
+        <Link to="/receitas-feitas">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Receitas Feitas
+          </button>
+        </Link>
+        <Link to="/receitas-favoritas">
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Receitas Favoritas
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ () => localStorage.clear() }
+          >
+            Sair
+          </button>
+        </Link>
+      </div>
       <Footer />
     </div>
   );
