2cff1250d6932f05f214557781a1190e3adee972
Hudson Carlos <hc88806@gmail.com>
2021-05-06 19:41:44 -0400

    58 59 done

--- src/Pages/RecipesDone.js
+++ src/Pages/RecipesDone.js
@@ -1,17 +1,205 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
-import ButtonAll from '../Components/ButtonAll';
-import ButtonFood from '../Components/ButtonFood';
-import ButtonDrink from '../Components/ButtonDrink';
+import '../Pages/Details+css/Details.css'
 
 class RecipesDone extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      doneRecipes: [],
+      food: [],
+      drink: [],
+      bebida: false,
+      comida: false,
+      all: true
+    };
+    this.all = this.all.bind(this)
+    this.renderComidas = this.renderComidas.bind(this);
+    this.renderBebidas = this.renderBebidas.bind(this);
+  }
+
+  componentDidMount() {
+    const recipesDone = JSON.parse(localStorage.getItem('doneRecipes'));
+    const filterFood = recipesDone.filter((food) => food.type === 'comida');
+    const filterDrink = recipesDone.filter((drink) => drink.type === 'bebida');
+    this.setState({
+      doneRecipes: recipesDone,
+      food: filterFood,
+      drink: filterDrink,
+    });
+  }
+
+  renderComidas() {
+    const { food, comida } = this.state;
+    if (comida) {
+      return (
+        <div>
+          { food.map((value, index) => (
+            <div key={ index }>
+              <Link to={`/comidas/${value.id}`}>
+                <img
+                  className="Imagem"
+                  data-testid={ `${index}-horizontal-image` }
+                  src={ value.image }
+                  alt="img-recipe"
+                />
+                <p
+                  data-testid={ `${index}-horizontal-name` }
+                >
+                  {value.name}
+                </p>
+                </Link>
+                <p
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  { value.category }
+                </p>
+              <p
+                data-testid={ `${index}-horizontal-done-date` }
+              >
+                {value.doneDate}
+              </p>
+              <span
+                data-testid={ `${index}-horizontal-tag` }
+              >
+                {value.tags[0]}
+              </span>
+            </div>
+          ))}
+        </div>
+      );
+    }
+    return false;
+  }
+
+  renderBebidas() {
+    const { drink, bebida } = this.state;
+    if (bebida) {
+      return (
+        <div>
+          { drink.map((value, index) => (
+            <div key={ index }>
+              <Link to={`/bebidas/${value.id}`}>
+                <img
+                  className="Imagem"
+                  data-testid={ `${index}-horizontal-image` }
+                  src={ value.image }
+                  alt="img-recipe"
+                />
+                <p
+                  data-testid={ `${index}-horizontal-name` }
+                >
+                  {value.name}
+                </p>
+                </Link>
+                <p
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  { value.category }
+                </p>
+              <p
+                data-testid={ `${index}-horizontal-done-date` }
+              >
+                {value.doneDate}
+              </p>
+              <span
+                data-testid={ `${index}-horizontal-tag` }
+              >
+                {value.tags[0]}
+              </span>
+            </div>
+          ))}
+        </div>
+      );
+    }
+    return false;
+  }
+
+  all() {
+    const { bebida, comida, doneRecipes } = this.state;
+    if (bebida === false && comida === false) {
+      return (
+        <div>
+          { doneRecipes.map((value, index) => (
+            <div key={ index }>
+              <Link to={`/${value.type}s/${value.id}`}>
+                <img
+                  className="Imagem"
+                  data-testid={ `${index}-horizontal-image` }
+                  src={ value.image }
+                  alt="img-recipe"
+                />
+                <p
+                  data-testid={ `${index}-horizontal-name` }
+                >
+                  {value.name}
+                </p>
+             </Link>
+                <p
+                  data-testid={ `${index}-horizontal-top-text` }
+                  >
+                  { value.category }
+                </p>
+              <p
+                data-testid={ `${index}-horizontal-done-date` }
+              >
+                {value.doneDate}
+              </p>
+              <span
+                data-testid={ `${index}-horizontal-tag` }
+              >
+                {value.tags[0]}
+              </span>
+            </div>
+          ))}
+        </div>
+      );
+    }
+    return null;
+  }
+
   render() {
     return (
       <div>
-        <Header name="Receitas Feitas" />
-        <ButtonAll />
-        <ButtonFood />
-        <ButtonDrink />
+        {/* <Header name="Receitas Feitas" /> */}
+        <button
+          type="button"
+          onClick={ () => this.setState({
+            comida: false,
+            bebida: false,
+            all: true,
+          }) }
+          data-testid="filter-by-all-btn"
+        >
+          All
+        </button>
+        <button
+          type="button"
+          onClick={ () => this.setState({
+            comida: false,
+            bebida: true,
+            all: false,
+          }) }
+          data-testid="filter-by-drink-btn"
+        >
+          Drinks
+        </button>
+        <button
+          type="button"
+          onClick={ () => this.setState({
+            comida: true,
+            bebida: false,
+            all: false,
+          }) }
+          data-testid="filter-by-food-btn"
+        >
+          Food
+        </button>
+        { this.renderComidas() }
+        { this.renderBebidas() }
+        { this.all() }
       </div>
     );
   }
