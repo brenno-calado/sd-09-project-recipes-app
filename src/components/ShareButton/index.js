36273d4df1ea2543bc930adacea72a749de61f87
Thiago Granville <thiagogranville@gmail.com>
2021-05-06 13:05:58 -0300

    [REFACTOR][Share Button][Implementa nova lógica no botão de share]

--- src/components/ShareButton/index.js
+++ src/components/ShareButton/index.js
@@ -5,10 +5,16 @@ import RecipesContext from '../../context/RecipesContext';
 import shareIcon from '../../images/shareIcon.svg';
 import './ShareButton.css';
 
-const ShareButton = () => {
+const ShareButton = (props) => {
   const { location } = useContext(RecipesContext);
   const [copied, setCopied] = useState(false);
-  const URL = `http://localhost:3000${location.pathname}`;
+  // const URL = `http://localhost:3000${location.pathname}`;
+
+  let URL = `http://localhost:3000${location.pathname}`
+
+  if(location.pathname === "/receitas-feitas" || location.pathname === "/receitas-favoritas") {
+    URL = `http://localhost:3000/${props.recipeType}/${props.recipeId}`;
+  }
 
   const copyUrl = () => {
     const sucessPromise = copy(URL);
