ba236be2f9235f43025f6cce6a1fdea99ef25161
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-05-06 09:39:33 -0300

    Adiciona todos os data-testid req 54

--- src/pages/ReceitasFeitas.js
+++ src/pages/ReceitasFeitas.js
@@ -2,9 +2,27 @@ import React from 'react';
 import Header from '../components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const doneRecipes = [{ tags: ['Pasta', 'Curry'] }, { tags: [] }];
+
 function ReceitasFeitas() {
   return (
-    <Header title="Receitas Feitas" showExplorerButton={ false } />
+    <div>
+      <Header title="Receitas Feitas" showExplorerButton={ false } />
+      <button data-testid="filter-by-all-btn" type="button">All</button>
+      <button data-testid="filter-by-food-btn" type="button">Food</button>
+      <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
+      {doneRecipes.map((item, index) => (
+        <section key={ index }>
+          <img data-testid={ `${index}-horizontal-image` } src="" alt="" />
+          <span data-testid={ `${index}-horizontal-top-text` } />
+          <h3 data-testid={ `${index}-horizontal-name` }>Name</h3>
+          <span data-testid={ `${index}-horizontal-done-date` } />
+          <span data-testid={ `${index}-horizontal-share-btn` } />
+          {item.tags.map((tag) => (
+            <span key={ tag } data-testid={ `${index}-${tag}-horizontal-tag` } />))}
+        </section>
+      ))}
+    </div>
   );
 }
 
