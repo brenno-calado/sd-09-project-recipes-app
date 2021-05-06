949e6181729f04253af83bce8ace8bdf2a89151e
MarcioFujita <hanker78@gmail.com>
2021-05-06 16:54:27 -0300

    Requisitos 54 a 59 v4

--- src/components/CardInProgress/index.jsx
+++ src/components/CardInProgress/index.jsx
@@ -35,6 +35,7 @@ function CardeInProgress({
   };
 
   // const [isBtnDisabled, setIsBtnDisabled] = useState(true);
+
   const [handleClickRedirect, shouldRedirect] = useShouldRedirect();
   const [copyUrl, handleClickUrl] = useHandleClickUrl();
 
