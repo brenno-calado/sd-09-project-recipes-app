ec8e08eb8dd592c68f089a45d6204e78692c20f2
Thiago Granville <thiagogranville@gmail.com>
2021-05-06 16:25:29 -0300

    [FEAT][Requisitos 63, 65, 66][Implementa parte da lógica da favorite page]

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../components/Header';
+import { loadFavoriteRecipesFromLocalStorage } from '../services/favoriteRecipes';
+import CardRecipe from '../components/CardRecipe';
+import useFilterType from '../hooks/useFilterType';
+import FilterRecipes from '../components/FilterRecipes';
 
-const FavoriteRecipes = () => (
-  <div>
-    <Header title="Receitas Favoritas" />
-  </div>
-);
+const FavoriteRecipes = () => {
+  const [favoriteRecipes] = useState(loadFavoriteRecipesFromLocalStorage());
+  const [fiteredFavRecipes, setFilterRecipesByType] = useFilterType(favoriteRecipes);
+  const [isFavorite] = useState(true);
+
+  const filterButton = ({ target }) => {
+    setFilterRecipesByType(target.innerText, favoriteRecipes);
+  };
+
+  return (
+    <div>
+      <Header title="Receitas Favoritas" />
+      <FilterRecipes filterButton={ filterButton } />
+      <CardRecipe filteredRecipes={ fiteredFavRecipes } isFavorite={ isFavorite } />
+    </div>
+  );
+};
 
 export default FavoriteRecipes;
