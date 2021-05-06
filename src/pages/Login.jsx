ac453a8c450a6a1ced0e6a0f2a1d7f6c926c1f6d
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-05-06 18:34:37 -0300

    Feat: requisito 87

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,7 @@ import '../styles/ButtonGreen.css';
 
 function Login() {
   const [form, setForm] = useState({
-    email: '', // começa vazio. Se tiver o e-mail, tem que manter, caso não, tem que colocar
+    email: '',
     password: '',
   });
   const [disable, setDisable] = useState(true);
