16f4f5971bd3d53fc7512ad58de4f726e0d7e79a
Renato Feitosa <pythaqua@gmail.com>
2021-05-06 19:28:25 -0300

    fix: Corrige código em RecipeList

--- src/pages/RecipeList.js
+++ src/pages/RecipeList.js
@@ -50,38 +50,13 @@ const renderButtons = (buttonParams, setFilterKey) => (
   ))
 );
 
-const doneRecipes = [
-  {
-    id: '52771',
-    type: 'comida',
-    area: 'Italian',
-    category: 'Vegetarian',
-    alcoholicOrNot: '',
-    name: 'Spicy Arrabiata Penne',
-    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-    doneDate: '23/06/2020',
-    tags: ['Pasta', 'Curry'],
-  },
-  {
-    id: '178319',
-    type: 'bebida',
-    area: '',
-    category: 'Cocktail',
-    alcoholicOrNot: 'Alcoholic',
-    name: 'Aquamarine',
-    image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-    doneDate: '23/06/2020',
-    tags: [],
-  },
-];
-
 const RecipeList = ({ match: { path } }) => {
   const { pathname } = useLocation();
   const isDonePage = pathname === paths.DONE_RECIPES;
-  // const storageKey = isDonePage ? 'doneRecipes' : 'favoriteRecipes';
-  // const storageRecipes = JSON.parse(localStorage.getItem(storageKey)) || [];
+  const storageKey = isDonePage ? 'doneRecipes' : 'favoriteRecipes';
+  const storageRecipes = JSON.parse(localStorage.getItem(storageKey)) || [];
 
-  const [recipes, setRecipesList] = useState(doneRecipes);
+  const [recipes, setRecipesList] = useState(storageRecipes);
   const [showCopyMsg, setShowCopyMsg] = useState(false);
   const [recipesFilterKey, setFilterKey] = useState('all');
 
