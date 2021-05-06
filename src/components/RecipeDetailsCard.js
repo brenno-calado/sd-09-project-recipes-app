769aea34444cdbba887f858d2783dcee4c4b017d
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-05-06 14:47:39 -0300

    iniciando requisito

--- src/components/RecipeDetailsCard.js
+++ src/components/RecipeDetailsCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { AiOutlineShareAlt } from 'react-icons/ai';
 import { GrFavorite } from 'react-icons/gr';
@@ -6,6 +6,7 @@ import { useParams } from 'react-router';
 import ReactPlayer from 'react-player';
 import { getRecipesById } from '../services/api';
 import RecomendationList from './RecomedationsList';
+import { RecipiesContext } from '../context/RecipiesContext';
 
 const getIngredientsAndMeasure = (recipe) => {
   let ingredientsList = [];
@@ -23,6 +24,7 @@ const getIngredientsAndMeasure = (recipe) => {
 function RecipesDetailsCard({ isMeal }) {
   const [recipeDetails, setRecipeDetails] = useState(null);
   const { id } = useParams();
+  const { favoriteRecipes, setFavoriteRecipes } = useContext(RecipiesContext);
 
   useEffect(() => {
     async function getRecipe() {
@@ -31,6 +33,10 @@ function RecipesDetailsCard({ isMeal }) {
     }
     getRecipe();
   }, [id, isMeal, setRecipeDetails]);
+
+  function addFavorite() {
+    setFavoriteRecipes({ obj: "test" });
+  }
 
   const renderIngredientsAndMeasure = () => getIngredientsAndMeasure(recipeDetails)
     .map(({ ingredient, measure }, index) => (
@@ -58,7 +64,11 @@ function RecipesDetailsCard({ isMeal }) {
       <button type="button" data-testid="share-btn">
         <AiOutlineShareAlt />
       </button>
-      <button type="button" data-testid="favorite-btn">
+      <button
+        type="button"
+        data-testid="favorite-btn"
+        onClick={ () => addFavorite() }
+      >
         <GrFavorite />
       </button>
       <h3 data-testid="recipe-category">
--- src/pages/ReceitasFeitas.js
+++ src/pages/ReceitasFeitas.js
@@ -4,7 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function ReceitasFeitas() {
   return (
-    <Header title="Receitas Feitas" showExplorerButton={ false } />
+    <div>
+      <Header title="Receitas Feitas" showExplorerButton={ false } />
+    </div>
   );
 }
 
