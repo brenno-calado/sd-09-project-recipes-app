ee35a3df60ebdfc2200d1512dc086a1adb03dd0d
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-05-06 10:57:40 -0300

    Adiciona botões de filtro em receitas feitas e redireciona para detalhes

--- src/pages/ReceitasFeitas.js
+++ src/pages/ReceitasFeitas.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import ShareButton from '../components/ShareRecipeButton';
 
@@ -12,26 +13,57 @@ function getHorizontalTopText({ type, area, category, alcoholicOrNot }) {
   return type === 'comida' ? `${area} - ${category}` : `${alcoholicOrNot}`;
 }
 
+function filterRecipesBy(recipeType) {
+  return ({ type }) => recipeType === '' || recipeType === type;
+}
+
+function getRedirectUrl({ id, type }) {
+  return type === 'comida' ? `/comidas/${id}` : `/bebidas/${id}`;
+}
+
 function ReceitasFeitas() {
   const [doneRecipes] = useState(getDoneRecipes());
-
+  const [filterBy, setFilterBy] = useState('');
   return (
     <div>
       <Header title="Receitas Feitas" showExplorerButton={ false } />
-      <button data-testid="filter-by-all-btn" type="button">All</button>
-      <button data-testid="filter-by-food-btn" type="button">Food</button>
-      <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
-      {doneRecipes.map((recipe, index) => (
+      <button
+        data-testid="filter-by-all-btn"
+        onClick={ () => setFilterBy('') }
+        type="button"
+      >
+        All
+      </button>
+      <button
+        data-testid="filter-by-food-btn"
+        onClick={ () => setFilterBy('comida') }
+        type="button"
+      >
+        Food
+      </button>
+      <button
+        data-testid="filter-by-drink-btn"
+        onClick={ () => setFilterBy('bebida') }
+        type="button"
+      >
+        Drinks
+      </button>
+      {doneRecipes.filter(filterRecipesBy(filterBy)).map((recipe, index) => (
         <section key={ index }>
-          <img
-            data-testid={ `${index}-horizontal-image` }
-            src={ recipe.image }
-            alt="Imagem da receita"
-          />
+          <Link to={ getRedirectUrl(recipe) }>
+            <img
+              width="100"
+              data-testid={ `${index}-horizontal-image` }
+              src={ recipe.image }
+              alt="Imagem da receita"
+            />
+          </Link>
           <span data-testid={ `${index}-horizontal-top-text` }>
             { getHorizontalTopText(recipe) }
           </span>
-          <h3 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h3>
+          <Link to={ getRedirectUrl(recipe) }>
+            <h3 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h3>
+          </Link>
           <span data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</span>
           <ShareButton
             isMeal={ recipe.type === 'comida' }
