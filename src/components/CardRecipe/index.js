9d90fefc4bfc56abee86144f360bd1500fd1df45
lcgoandete <lcgoandete@hotmail.com>
2021-05-06 08:41:30 -0300

    [FEAT][Requisito 58][implementando botoes de filtro]

---
+++ src/components/CardRecipe/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { arrayOf } from 'prop-types';
+
+import ShareButton from '../ShareButton';
+
+const CardRecipe = (props) => {
+  const { filteredRecipes } = props;
+
+  return (
+    <div>
+      {
+        filteredRecipes.map((recipe, index) => (
+          <div key={ recipe.name }>
+            <img
+              src={ recipe.image }
+              alt="imagem da Receita"
+              data-testid={ `${index}-horizontal-image` }
+              height="250"
+              width="200"
+            />
+            <p data-testid={ `${index}-horizontal-top-text` }>
+              { recipe.alcoholicOrNot || recipe.category }
+            </p>
+            <p data-testid={ `${index}-horizontal-name` }>
+              { recipe.name }
+            </p>
+            <p data-testid={ `${index}-horizontal-done-date` }>
+              { `Feita em: ${recipe.doneDate}` }
+            </p>
+            <ShareButton data-testid={ `${index}-horizontal-share-btn` } />
+            { recipe.tags.map((tag) => <span key={ tag }>{ tag }</span>) }
+          </div>
+        ))
+      }
+    </div>
+  );
+};
+
+CardRecipe.propTypes = {
+  filteredRecipes: arrayOf({}),
+}.isRequeired;
+
+export default CardRecipe;
