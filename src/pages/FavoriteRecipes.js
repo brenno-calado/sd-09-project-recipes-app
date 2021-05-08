c8bcbecfc0a0d77da9ed0308fbc44a62b886b801
Khyrki <khyrkimitsunobi@gmail.com>
2021-05-07 22:55:07 -0300

    Add requisito 60,61,62

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import FavoriteRecipeCard from '../components/FavoriteRecipeCard';
 
 class ReceitasFavoritas extends Component {
   render() {
     const searchIcon = false;
+    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    console.log(favoriteRecipes);
     return (
-      <Header title="Receitas Favoritas" searchIcon={ searchIcon } />
+      <div>
+        <Header title="Receitas Favoritas" searchIcon={ searchIcon } />
+        <button
+          data-testid="filter-by-all-btn"
+          type="button"
+        >
+          All
+        </button>
+        <button
+          data-testid="filter-by-food-btn"
+          type="button"
+        >
+          Food
+        </button>
+        <button
+          data-testid="filter-by-drink-btn"
+          type="button"
+        >
+          Drinks
+        </button>
+        { favoriteRecipes.map((recipe, index) => (
+          <FavoriteRecipeCard
+            key={ recipe.id }
+            recipe={ recipe }
+            index={ index }
+          />)) }
+      </div>
     );
   }
 }
