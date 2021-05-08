5fda0bb1e0a8712f78cfeba0fc24c7e018a32954
ViniciusChagas <viniciushrcs@gmail.com>
2021-05-08 13:12:26 -0300

    Lógica para renderizar as receitas favoritas criada

--- src/Pages/TelaFavoritos.js
+++ src/Pages/TelaFavoritos.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FavoriteCard from '../components/FavoriteCard';
 
-export default function FavoritesPage() {
+export default function TelaFavoritos() {
+  const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  console.log(favoriteRecipes);
+  const [filter, setFilter] = useState('');
   return (
     <div>
-      Olá
+      <button
+        data-testid="filter-by-all-btn"
+        type="button"
+        // onClick={ setFilter('') }
+      >
+        All
+      </button>
+      <button
+        data-testid="filter-by-food-btn"
+        type="button"
+        // onClick={ setFilter('comida') }
+      >
+        Food
+      </button>
+      <button
+        data-testid="filter-by-drink-btn"
+        type="button"
+        // onClick={ setFilter('bebida') }
+      >
+        Drinks
+      </button>
+      {favoriteRecipes.map((element, index) => (
+        < FavoriteCard index={ index } data={ element } />
+      ))}
     </div>
   );
 }
-
-//filtro foodOrDrink que pode ser Meal, Drink, All
-
-//Card minimizado para food ou drink
-
-//funcao de compartilhar
--- src/components/FavoriteCard.js
+++ src/components/FavoriteCard.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import FavoriteButton from './FavoriteButton';
 import ShareButton from './ShareButton';
 
-export default function FavoriteCard() {
+const FavoriteCard = (props) => {
+  const { index, data } = props;
+  const {
+    id,
+    type,
+    category,
+    alcoholicOrNot,
+    image,
+    name,
+  } = data;
   return (
     <div>
+      <p>
+        {type === 'comida' ? category : alcoholicOrNot}
+      </p>
+      <h1 data-testid={ `${index}-horizontal-name` }>
+        { name }
+      </h1>
       <img
         className="recipe-image"
-        src={ data[`str${foodOrDrink}Thumb`] }
-        alt={ data[`str${foodOrDrink}`] }
-        data-testid="recipe-photo"
+        src={ image }
+        alt={ name }
+        data-testid={ `${index}-horizontal-image` }
         width="100"
         height="100"
       />
       <ShareButton />
+      <FavoriteButton />
     </div>
   );
-}
+};
+export default FavoriteCard;
