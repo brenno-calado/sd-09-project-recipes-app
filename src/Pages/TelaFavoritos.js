e6651cb47f3b51a4be4b2189d8b492cc1e82540c
ViniciusChagas <viniciushrcs@gmail.com>
2021-05-08 17:43:08 -0300

    Filtro funcionando

--- src/Pages/TelaFavoritos.js
+++ src/Pages/TelaFavoritos.js
@@ -4,33 +4,37 @@ import FavoriteCard from '../components/FavoriteCard';
 export default function TelaFavoritos() {
   const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
   console.log(favoriteRecipes);
-  const [filter, setFilter] = useState('');
+  const [filter, setFilter] = useState(null);
+
   return (
     <div>
       <button
         data-testid="filter-by-all-btn"
         type="button"
-        // onClick={ setFilter('') }
+        onClick={ () => setFilter(null) }
       >
         All
       </button>
       <button
         data-testid="filter-by-food-btn"
         type="button"
-        // onClick={ setFilter('comida') }
+        onClick={ () => setFilter('comida') }
       >
         Food
       </button>
       <button
         data-testid="filter-by-drink-btn"
         type="button"
-        // onClick={ setFilter('bebida') }
+        onClick={ () => setFilter('bebida') }
       >
         Drinks
       </button>
-      {favoriteRecipes.map((element, index) => (
-        < FavoriteCard index={ index } data={ element } />
-      ))}
+      {filter ? favoriteRecipes
+        .filter((element) => element.type === filter)
+        .map((element, index) => (
+          <FavoriteCard key={ index } data={ element } />))
+        : favoriteRecipes.map((element, index) => (
+          <FavoriteCard key={ index } data={ element } />))}
     </div>
   );
 }
