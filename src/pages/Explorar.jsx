77a6a460a2b68428ec85cb38206f6f29b1fd2c18
Raphael Rêgo <raphael.mlrego@gmail.com>
2021-05-08 17:08:27 -0300

    pagina de explorar finalizada

--- src/pages/Explorar.jsx
+++ src/pages/Explorar.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 
 function Explorar() {
   return (
     <div>
+      <Link to="/explorar/comidas">
+        <button type="button" data-testid="explore-food">Explorar Comidas</button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
+      </Link>
       <Footer />
     </div>
   );
