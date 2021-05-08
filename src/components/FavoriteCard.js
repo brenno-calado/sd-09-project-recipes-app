4fe27f77b8211c5fe7d75b41994ebc5741f22b53
ViniciusChagas <viniciushrcs@gmail.com>
2021-05-08 19:31:40 -0300

    FavoriteCard alterado

--- src/components/FavoriteCard.js
+++ src/components/FavoriteCard.js
@@ -1,7 +1,9 @@
 import React, { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import FavoriteButton from './FavoriteButton';
-import ShareButton from './ShareButton';
+import ShareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import { saveAsFavorite } from '../services/details';
 import { MyContext } from '../MyContext';
 
@@ -15,30 +17,57 @@ const FavoriteCard = (props) => {
     alcoholicOrNot,
     image,
     name,
+    area,
   } = element;
   const [isFavorite, setIsFavorite] = useState(true);
+  const [isCopied, setIsCopied] = useState(false);
+
   const saveFavorite = () => {
     setIsFavorite(!isFavorite);
     saveAsFavorite(id, data, type);
   };
+
+  const copyToClipboard = () => {
+    const url = `http://localhost:3000/${type}s/${id}`;
+    navigator.clipboard.writeText(url);
+    setIsCopied(true);
+  };
+
   return (
     <div>
-      <p>
-        {type === 'comida' ? category : alcoholicOrNot}
+      <p data-testid={ `${index}-horizontal-top-text` }>
+        {type === 'comida' ? `${area} - ${category}` : alcoholicOrNot}
       </p>
-      <h1 data-testid={ `${index}-horizontal-name` }>
-        { name }
-      </h1>
-      <img
-        className="recipe-image"
-        src={ image }
-        alt={ name }
-        data-testid={ `${index}-horizontal-image` }
-        width="100"
-        height="100"
-      />
-      <ShareButton />
-      <FavoriteButton onClick={ saveFavorite } isFavorite={ isFavorite } />
+      <Link to={ `/${type}s/${id}` }>
+        <h1 data-testid={ `${index}-horizontal-name` }>
+          { name }
+        </h1>
+      </Link>
+      <Link to={ `/${type}s/${id}` }>
+        <img
+          className="recipe-image"
+          src={ image }
+          alt={ name }
+          data-testid={ `${index}-horizontal-image` }
+          width="100"
+          height="100"
+        />
+      </Link>
+      <button type="button" onClick={ copyToClipboard }>
+        { isCopied ? 'Link copiado!' : <img
+          src={ ShareIcon }
+          data-testid={ `${index}-horizontal-share-btn` }
+          alt="share-icon"
+        />}
+      </button>
+      <button type="button" onClick={ saveFavorite }>
+        <img
+          type="button"
+          data-testid={ `${index}-horizontal-favorite-btn` }
+          src={ isFavorite ? blackHeartIcon : whiteHeartIcon }
+          alt="favorite icon"
+        />
+      </button>
     </div>
   );
 };
@@ -51,6 +80,7 @@ FavoriteCard.propTypes = {
     alcoholicOrNot: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
+    area: PropTypes.string.isRequired,
   })).isRequired,
 };
 export default FavoriteCard;
