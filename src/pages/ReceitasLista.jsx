d85ba795c20d51ac930abb495e1028ad1957220f
Raquel Picanço <picanco.raquel@gmail.com>
2021-05-09 10:04:43 -0400

    Cria estilo para parte de ReceitasLista

--- src/pages/ReceitasLista.jsx
+++ src/pages/ReceitasLista.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react';
 import CardDoneFavorite from '../components/CardDoneFavorite';
 import Header from '../components/Header';
 import { RecipesContext } from '../context';
+import '../styles/ReceitasLista.css';
 
 function ReceitasLista() {
   const {
@@ -16,11 +17,13 @@ function ReceitasLista() {
       <button type="button" data-testid="filter-by-all-btn">All</button>
       <button type="button" data-testid="filter-by-food-btn">Food</button>
       <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
-      {
-        favoriteRecipes.map((recipe, index) => (
-          <CardDoneFavorite data={ recipe } index={ index } key={ recipe.name } />
-        ))
-      }
+      <section className="cards-done">
+        {
+          favoriteRecipes.map((recipe, index) => (
+            <CardDoneFavorite data={ recipe } index={ index } key={ recipe.name } />
+          ))
+        }
+      </section>
     </>
   );
 }
