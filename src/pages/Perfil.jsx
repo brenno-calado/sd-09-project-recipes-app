a50e8aa0fb44947ce6a96d6be5b57cbead4b0ed9
Matheus Monteiro <monteiromsh@gmail.com>
2021-05-09 18:27:35 -0300

    Requisito - Perfil

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 
 function Perfil() {
   return (
     <div>
+      <p data-testid="profile-email">email@mail.com</p>
+      <Link to="/receitas-feitas">
+        <button data-testid="profile-done-btn" type="button">
+          Receitas Feitas
+        </button>
+      </Link>
+
+      <Link to="/receitas-favoritas">
+        <button data-testid="profile-favorite-btn" type="button">
+          Receitas Favoritas
+        </button>
+      </Link>
+
+      <Link to="/">
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+          onClick={ () => localStorage.clear() }
+        >
+          Sair
+        </button>
+      </Link>
+
       <Footer />
     </div>
   );
