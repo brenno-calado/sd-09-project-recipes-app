8497ac3b1581cd2d40e19716b2c01cf2a6d16a57
Micaela Alves <micaela.alves93@gmail.com>
2021-05-10 05:28:06 -0300

    req 75 ok

--- src/pages/ExplorarIngredientes.jsx
+++ src/pages/ExplorarIngredientes.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { useLocation, useHistory } from 'react-router-dom';
+import React, { useEffect, useState/* , useContext */ } from 'react';
+import { useLocation/* , useHistory */ } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import getIngredients from '../services/ingredientsAPI';
-import Loading from '../components/Loading';
+/* import Loading from '../components/Loading'; */
 import '../styles/ExplorarIngredientes.css';
-import { RecipesContext } from '../context';
+// import { RecipesContext } from '../context';
 
 function ExplorarIngredientes() {
   const [ingredientsList, setIngredientsList] = useState();
@@ -15,9 +15,10 @@ function ExplorarIngredientes() {
     : ['bebidas', 'strIngredient1'];
   const imgURL = 'https://www.themealdb.com/images/ingredients/';
   const [loading, setLoading] = useState(true);
+
   const MAX_PG = 12;
-  const history = useHistory();
-  const { actions: { setIngredient } } = useContext(RecipesContext);
+  // const history = useHistory();
+  // const { actions: { setIngredient } } = useContext(RecipesContext);
 
   useEffect(() => {
     const loadIngredients = () => {
@@ -29,17 +30,17 @@ function ExplorarIngredientes() {
         .finally(() => setLoading(false));
     };
     loadIngredients();
-  });
+  }, []);
 
-  const handleClick = (searchIng) => {
+  /*   const handleClick = (searchIng) => {
     console.log(searchIng);
     setIngredient(searchIng);
     history.push('/comidas');
   };
 
-  function renderCardIngredients() {
+  /*   function renderCardIngredients() {
     return ingredientsList
-      /* .filter((_, index) => index < MAX_PG) */
+      .filter((_, index) => index < MAX_PG)
       .map((ing, index) => (
         <section
           key={ ing[type[1]] }
@@ -50,7 +51,7 @@ function ExplorarIngredientes() {
           aria-hidden="true"
         >
           <img
-            src={ `${imgURL}${ing[type[1]]}.png` }
+            src={ `${imgURL}${ing[type[1]]}-Small.png` }
             alt={ `imagem de ${ing}` }
             data-testid={ `${index}-card-img` }
             className=".card__image"
@@ -61,15 +62,45 @@ function ExplorarIngredientes() {
 
         </section>
       ));
+  } */
+
+  function loadCocktails() {
+
+  }
+
+  function loadMeals() {
+
   }
 
   return !loading ? (
     <>
       <Header />
-      { renderCardIngredients() }
+      { ingredientsList
+        .filter((_, index) => index < MAX_PG)
+        .map((ing, index) => (
+          <section
+            key={ ing[type[1]] }
+            data-testid={ `${index}-ingredient-card` }
+            className="recipe-card"
+            /* onClick={ () => handleClick(ing[type[1]]) } */
+            role="link"
+            aria-hidden="true"
+          >
+            <img
+              src={ `${imgURL}${ing[type[1]]}-Small.png` }
+              alt={ `imagem de ${ing}` }
+              data-testid={ `${index}-card-img` }
+              className=".card__image"
+            />
+            <p data-testid={ `${index}-card-name` }>
+              {ing[type[1]]}
+            </p>
+
+          </section>
+        ))}
       <Footer />
     </>
-  ) : <Loading />;
+  ) : <p>Carregando...</p>;
 }
 
 export default ExplorarIngredientes;
