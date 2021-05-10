312aa3b9b438a3781ab2084d4cef972c9d4a455a
Micaela Alves <micaela.alves93@gmail.com>
2021-05-10 19:59:02 -0300

    linter

--- src/tests/ReceitasPrincipal.test.js
+++ src/tests/ReceitasPrincipal.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import renderWithRouter from '../renderWithRouter';
 import ReceitasPrincipal from '../pages/ReceitasPrincipal';
-import App from '../App';
 
 describe('2 - Teste o componente ReceitasPrincipal.js', () => {
   it('A rota "/comidas" renderiza ReceitasPrincipal.js', () => {
-    const { history } = renderWithRouter(<App />);
+    const { history } = renderWithRouter(<ReceitasPrincipal />);
     const { pathname } = history.location;
     const urlApp = pathname;
     expect(urlApp).toBe('/comidas');
