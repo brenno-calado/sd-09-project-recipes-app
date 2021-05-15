0c113f7fb52e32bcf14e1fcc5325cfe8fdf783af
Diogenes <diogenesfelipe.s@outlook.com>
2021-05-15 20:39:24 -0300

    feat/Style Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,47 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Context } from '../context';
 import { fecthByName } from '../services/api';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
+import '../css/Footer.css';
 
 function Footer() {
   const { updateData } = useContext(Context);
+  const { pathname } = useLocation();
 
-  const renderImage = (testid, src, alt) => (
-    <img data-testid={ testid } src={ src } alt={ alt } />
+  const renderImage = (testid, src, alt, className) => (
+    <img
+      data-testid={ testid }
+      src={ src }
+      alt={ alt }
+      className={ className }
+    />
   );
 
+  const inOrOutMealsPage = pathname.includes('comidas') ? 'in-page' : 'out-page';
+  const inOrOutExplorePage = pathname.includes('explorar') ? 'in-page' : 'out-page';
+  const inOrOutDrinksPage = pathname.includes('bebidas') ? 'in-page' : 'out-page';
+
   return (
-    <footer data-testid="footer">
-      <Link to="/bebidas" onClick={ () => updateData(fecthByName('', false)) }>
-        { renderImage('drinks-bottom-btn', drinkIcon, 'drink-icon') }
+    <footer data-testid="footer" className="footer-wrapper">
+      <Link
+        to="/comidas"
+        onClick={ () => updateData(fecthByName('', true)) }
+        className={ inOrOutMealsPage }
+      >
+        { renderImage('food-bottom-btn', mealIcon, 'meal-icon') }
       </Link>
-      <Link to="/explorar">
+      <Link to="/explorar" className={ inOrOutExplorePage }>
         { renderImage('explore-bottom-btn', exploreIcon, 'explore-icon') }
       </Link>
-      <Link to="/comidas" onClick={ () => updateData(fecthByName('', true)) }>
-        { renderImage('food-bottom-btn', mealIcon, 'meal-icon') }
+      <Link
+        to="/bebidas"
+        onClick={ () => updateData(fecthByName('', false)) }
+        className={ inOrOutDrinksPage }
+      >
+        { renderImage('drinks-bottom-btn', drinkIcon, 'drink-icon') }
       </Link>
     </footer>
   );
