2fd73ce7d7269b04e7b94ae7b0403ca9a8f4bbba
Diogenes <diogenesfelipe.s@outlook.com>
2021-05-16 01:16:19 -0300

    fix/remove imports

--- src/pages/meals/index.js
+++ src/pages/meals/index.js
@@ -1,5 +1,4 @@
 export { default as Meals } from './Meals';
-export { default as MealsDetails } from './MealsDetails';
 export { default as ExploreMeals } from './ExploreMeals';
 export { default as MealsByIngredients } from './MealsByIngredients';
 export { default as MealsByArea } from './MealsByArea';
