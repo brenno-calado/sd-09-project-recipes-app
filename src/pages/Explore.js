f4bfab27248e32d44f474e1531eefd7b653ff192
Diogenes <diogenesfelipe.s@outlook.com>
2021-05-22 14:37:24 -0300

    feat/style explore component

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaCocktail, FaUtensils } from 'react-icons/fa';
 import { Header, Footer } from '../components';
+import '../css/Explore.css';
 
 function Explore() {
   return (
     <section>
       <Header title="Explorar" />
-      <Link to="/explorar/comidas">
-        <button data-testid="explore-food" type="button">Explorar Comidas</button>
-      </Link>
-      <Link to="/explorar/bebidas">
-        <button data-testid="explore-drinks" type="button">Explorar Bebidas</button>
-      </Link>
+      <div className="buttons-explore-container">
+        <Link to="/explorar/comidas">
+          <button data-testid="explore-food" type="button">
+            <FaUtensils className="icon-btn" />
+            Explorar Comidas
+          </button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button data-testid="explore-drinks" type="button">
+            <FaCocktail className="icon-btn" />
+            Explorar Bebidas
+          </button>
+        </Link>
+      </div>
       <Footer />
     </section>
   );
