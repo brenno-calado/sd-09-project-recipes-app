176abde9ce0ac8510ac6c9af4c0d3d732199a8da
Diogenes <diogenesfelipe.s@outlook.com>
2021-05-23 14:09:46 -0300

    feat/style page Profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { Header, Footer } from '../components';
+import '../css/Profile.css';
 
 function Profile() {
   const [shouldRedirect, setShouldRedirect] = useState(false);
@@ -12,23 +13,38 @@ function Profile() {
     setShouldRedirect(true);
   };
 
-  const createButton = (testid, name, onClick) => (
-    <button data-testid={ testid } type="button" onClick={ onClick }>{ name }</button>
+  const createButton = (testid, name, colorButton, onClick) => (
+    <button
+      data-testid={ testid }
+      type="button"
+      onClick={ onClick }
+      className={ `button-profile ${colorButton}` }
+    >
+      { name }
+    </button>
   );
 
   if (shouldRedirect) return <Redirect to="/" />;
 
   return (
     <section>
-      <Header title="Perfil" />
-      <p data-testid="profile-email">{ email }</p>
-      <Link to="/receitas-feitas">
-        { createButton('profile-done-btn', 'Receitas Feitas') }
-      </Link>
-      <Link to="/receitas-favoritas">
-        { createButton('profile-favorite-btn', 'Receitas Favoritas') }
-      </Link>
-      { createButton('profile-logout-btn', 'Sair', handleLogout) }
+      <Header title="Perfil" profilePage />
+      <main className="main-profile">
+        <h4 className="title-data">Dados da conta</h4>
+        <div className="data-container">
+          <div className="data">
+            <span>Email: </span>
+            <p data-testid="profile-email">{email}</p>
+          </div>
+        </div>
+        <Link to="/receitas-feitas">
+          { createButton('profile-done-btn', 'Receitas Feitas', 'orange') }
+        </Link>
+        <Link to="/receitas-favoritas">
+          { createButton('profile-favorite-btn', 'Receitas Favoritas', 'blue') }
+        </Link>
+        { createButton('profile-logout-btn', 'Sair', 'red small-btn', handleLogout) }
+      </main>
       <Footer />
     </section>
   );
